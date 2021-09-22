6084ddd0e8ecab964e27e6577562c9bdddf08351
Ahmad Abdalla <28486158+ahmadabdalla@users.noreply.github.com>
2021-09-22 22:09:23 +1000

    uplifted new-deployment - attempt 1

--- .github/actions/templates/deployModule/scripts/New-ModuleDeployment.ps1
+++ .github/actions/templates/deployModule/scripts/New-ModuleDeployment.ps1
@@ -74,89 +74,124 @@ function New-ModuleDeployment {
     }
     
     process {
-        $DeploymentInputs = @{
-            Name                  = "$moduleName-$(-join (Get-Date -Format yyyyMMddTHHMMssffffZ)[0..63])"
-            TemplateFile          = $templateFilePath
-            TemplateParameterFile = $parameterFilePath
-            Verbose               = $true
-            ErrorAction           = 'Stop'
+
+        ## Assess Provided Parameter Path
+        if (Test-Path -Path $parameterFilePath -PathType Container) {
+            ## Transform Path to Files
+            $parameterFilePath = Get-ChildItem $parameterFilePath -Recurse -Filter *.parameters.json | Select-Object -ExpandProperty FullName
+            Write-Verbose "Parameter File(s)/Directory - Count: `n $($parameterFilePath.Count)`n"
         }
-        if ($removeDeployment) {
-            # Fetch tags of parameter file if any (- required for the remove process. Tags may need to be compliant with potential customer requirements)
-            $parameterFileTags = (ConvertFrom-Json (Get-Content -Raw -Path $parameterFilePath) -AsHashtable).parameters.tags.value
-            if (-not $parameterFileTags) {
-                $parameterFileTags = @{}
+
+        ## Iterate through each file
+        foreach ($file in $parameterFilePath) {
+            $fileProperties = Get-Item -Path $file
+            Write-Verbose "Deploying: $($fileProperties.Name) `n"
+            [bool]$Stoploop = $false
+            [int]$Retrycount = 0
+
+            $DeploymentInputs = @{
+                Name                  = "$moduleName-$(-join (Get-Date -Format yyyyMMddTHHMMssffffZ)[0..63])"
+                TemplateFile          = $templateFilePath
+                TemplateParameterFile = $file
+                Verbose               = $true
+                ErrorAction           = 'Stop'
             }
-            $parameterFileTags['removeModule'] = $moduleName
-        }
-        #######################
-        ## INVOKE DEPLOYMENT ##
-        #######################
-        $deploymentSchema = (ConvertFrom-Json (Get-Content -Raw -Path $templateFilePath)).'$schema'
-        switch -regex ($deploymentSchema) {
-            '\/deploymentTemplate.json#$' {
-                if ($subscriptionId) {
-                    $Context = Get-AzContext -ListAvailable | Where-Object Subscription -Match $subscriptionId
-                    if ($Context) {
-                        $Context | Set-AzContext
+            if ($removeDeployment) {
+                # Fetch tags of parameter file if any (- required for the remove process. Tags may need to be compliant with potential customer requirements)
+                $parameterFileTags = (ConvertFrom-Json (Get-Content -Raw -Path $file) -AsHashtable).parameters.tags.value
+                if (-not $parameterFileTags) {
+                    $parameterFileTags = @{}
+                }
+                $parameterFileTags['removeModule'] = $moduleName
+            }
+            #######################
+            ## INVOKE DEPLOYMENT ##
+            #######################
+            do {
+                try {
+                    $deploymentSchema = (ConvertFrom-Json (Get-Content -Raw -Path $templateFilePath)).'$schema'
+                    switch -regex ($deploymentSchema) {
+                        '\/deploymentTemplate.json#$' {
+                            if ($subscriptionId) {
+                                $Context = Get-AzContext -ListAvailable | Where-Object Subscription -Match $subscriptionId
+                                if ($Context) {
+                                    $Context | Set-AzContext
+                                }
+                            }
+                            if (-not (Get-AzResourceGroup -Name $resourceGroupName -ErrorAction 'SilentlyContinue')) {
+                                if ($PSCmdlet.ShouldProcess("Resource group [$resourceGroupName] in location [$location]", "Create")) {
+                                    New-AzResourceGroup -Name $resourceGroupName -Location $location
+                                }
+                            }
+                            if ($removeDeployment) {
+                                Write-Verbose "Because the subsequent removal is enabled after the Module $moduleName has been deployed, the following tags (removeModule: $moduleName) are now set on the resource."
+                                Write-Verbose "This is necessary so that the later running Removal Stage can remove the corresponding Module from the Resource Group again."
+                                # Overwrites parameter file tags parameter  
+                                $DeploymentInputs += @{ 
+                                    Tags = $parameterFileTags
+                                }
+                            }
+                            if ($PSCmdlet.ShouldProcess("Resource group level deployment", "Create")) {
+                                New-AzResourceGroupDeployment @DeploymentInputs -ResourceGroupName $resourceGroupName
+                            }
+                            break
+                        }
+                        '\/subscriptionDeploymentTemplate.json#$' {
+                            if ($subscriptionId) {
+                                $Context = Get-AzContext -ListAvailable | Where-Object Subscription -Match $subscriptionId
+                                if ($Context) {
+                                    $Context | Set-AzContext
+                                }
+                            }
+                            if ($removeDeployment) {
+                                Write-Verbose "Because the subsequent removal is enabled after the Module $moduleName has been deployed, the following tags (removeModule: $moduleName) are now set on the resource."
+                                Write-Verbose "This is necessary so that the later running Removal Stage can remove the corresponding Module from the Resource Group again."
+                                # Overwrites parameter file tags parameter  
+                                $DeploymentInputs += @{ 
+                                    Tags = $parameterFileTags
+                                }
+                            }
+                            if ($PSCmdlet.ShouldProcess("Subscription level deployment", "Create")) {
+                                New-AzSubscriptionDeployment @DeploymentInputs -location $location
+                            }
+                            break
+                        }
+                        '\/managementGroupDeploymentTemplate.json#$' {
+                            if ($PSCmdlet.ShouldProcess("Management group level deployment", "Create")) {
+                                New-AzManagementGroupDeployment @DeploymentInputs -location $location -managementGroupId $managementGroupId
+                            }
+                            break
+                        }
+                        '\/tenantDeploymentTemplate.json#$' {
+                            if ($PSCmdlet.ShouldProcess("Tenant level deployment", "Create")) {
+                                New-AzTenantDeployment @DeploymentInputs -location $location
+                            }
+                            break
+                        }
+                        default {
+                            throw "[$deploymentSchema] is a non-supported ARM template schema"
+                            $Stoploop = $true
+                        }
                     }
-                }
-                if (-not (Get-AzResourceGroup -Name $resourceGroupName -ErrorAction 'SilentlyContinue')) {
-                    if ($PSCmdlet.ShouldProcess("Resource group [$resourceGroupName] in location [$location]", "Create")) {
-                        New-AzResourceGroup -Name $resourceGroupName -Location $location
+                    $Stoploop = $true
+                } #end try
+                catch {
+                    if ($Retrycount -gt 1) {
+                        throw $PSitem.Exception.Message
+                        $Stoploop = $true
                     }
-                }
-                if ($removeDeployment) {
-                    Write-Verbose "Because the subsequent removal is enabled after the Module $moduleName has been deployed, the following tags (removeModule: $moduleName) are now set on the resource."
-                    Write-Verbose "This is necessary so that the later running Removal Stage can remove the corresponding Module from the Resource Group again."
-                    # Overwrites parameter file tags parameter  
-                    $DeploymentInputs += @{ 
-                        Tags = $parameterFileTags
+                    else {
+                        Write-Verbose "Resource deployment Failed.. ($Retrycount/1) Retrying in 5 Seconds.. `n"
+                        Start-Sleep -Seconds 5
+                        $Retrycount = $Retrycount + 1
                     }
-                }
-                if ($PSCmdlet.ShouldProcess("Resource group level deployment", "Create")) {
-                    New-AzResourceGroupDeployment @DeploymentInputs -ResourceGroupName $resourceGroupName
-                }
-                break
-            }
-            '\/subscriptionDeploymentTemplate.json#$' {
-                if ($subscriptionId) {
-                    $Context = Get-AzContext -ListAvailable | Where-Object Subscription -Match $subscriptionId
-                    if ($Context) {
-                        $Context | Set-AzContext
-                    }
-                }
-                if ($removeDeployment) {
-                    Write-Verbose "Because the subsequent removal is enabled after the Module $moduleName has been deployed, the following tags (removeModule: $moduleName) are now set on the resource."
-                    Write-Verbose "This is necessary so that the later running Removal Stage can remove the corresponding Module from the Resource Group again."
-                    # Overwrites parameter file tags parameter  
-                    $DeploymentInputs += @{ 
-                        Tags = $parameterFileTags
-                    }
-                }
-                if ($PSCmdlet.ShouldProcess("Subscription level deployment", "Create")) {
-                    New-AzSubscriptionDeployment @DeploymentInputs -location $location
-                }
-                break
-            }
-            '\/managementGroupDeploymentTemplate.json#$' {
-                if ($PSCmdlet.ShouldProcess("Management group level deployment", "Create")) {
-                    New-AzManagementGroupDeployment @DeploymentInputs -location $location -managementGroupId $managementGroupId
-                }
-                break
-            }
-            '\/tenantDeploymentTemplate.json#$' {
-                if ($PSCmdlet.ShouldProcess("Tenant level deployment", "Create")) {
-                    New-AzTenantDeployment @DeploymentInputs -location $location
-                }
-                break
-            }
-            default {
-                throw "[$deploymentSchema] is a non-supported ARM template schema"
-            }
-        }
-    }
-    
+                } #end catch
+            } #end do
+            while ($Stoploop -eq $false) { 
+            } #end while
+        } #end foreach parameter file
+    } #end process
+
     end {
         Write-Debug ("{0} exited" -f $MyInvocation.MyCommand)  
     }
