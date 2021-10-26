b5e7604a96c45f9f91b23d837840aff2003df480
Ahmad Abdalla <28486158+ahmadabdalla@users.noreply.github.com>
2021-10-26 22:52:33 +1100

    '#[<subscriptionId>]#'

--- arm/.global/global.module.tests.ps1
+++ arm/.global/global.module.tests.ps1
@@ -724,12 +724,12 @@ Describe 'Deployment template tests' -Tag Template {
             foreach ($parameterFileTestCase in $parameterFileTestCases) {
                 $parameterFileContent = Get-Content -Path $parameterFileTestCase.parameterFile_Path
                 if (($parameterFileContent | Select-String -Pattern '/subscriptions/' -Quiet) -or ($parameterFileContent | Select-String -Pattern '"subscriptionId"' -Quiet)) {
-                    if (!($parameterFileContent | Select-String -Pattern '[<subscriptionId>]')) {
+                    if (!($parameterFileContent | Select-String -Pattern "'`#[<subscriptionId>]`#'")) {
                         $SubIdVisible = $true
                     }
                 }
 
-                $SubIdVisible | Should -Be $null -Because ('Parameter file should not contain original subscription Id value, but a case sensitive token value [<subscriptionId>]')
+                $SubIdVisible | Should -Be $null -Because ('Parameter file should not contain original subscription Id value, but a token value "`#[<subscriptionId>]`#"')
             }
         }
     }
