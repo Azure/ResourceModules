d55545e5a961983354d8178e5d506ace01d77d86
Marius <Marius.Storhaug@microsoft.com>
2021-10-29 18:06:11 +0200

    Fix global module test

--- .github/actions/templates/getWorkflowInput/scripts/Get-WorkflowDefaultInput.ps1
+++ .github/actions/templates/getWorkflowInput/scripts/Get-WorkflowDefaultInput.ps1
@@ -22,30 +22,23 @@ function Get-WorkflowDefaultInput {
 
     begin {
         Write-Debug ('{0} entered' -f $MyInvocation.MyCommand)
+        Install-Module -Name 'powershell-yaml' -Force
     }
 
     process {
-        Install-Module -Name powershell-yaml -Repository PSGallery -Force -Verbose
-        Import-Module -Name powershell-yaml -Verbose
         #$content = Get-Content $workflowPath
-        $content = Get-Content $workflowPath -Raw
+        $content = Get-Content -Path $workflowPath -Raw
         $obj = ConvertFrom-Yaml -Yaml $content
 
         # Get 'removeDeployment' default input value
-        # $removeDeploymentRowIndex = ((0..($content.Count - 1)) | Where-Object { $content[$_] -like '*removeDeployment:*' })[0]
-        # $removeDeployment = $content[$removeDeploymentRowIndex + 3].trim().Split(':')[1].Trim().Replace("'", '').Replace('"', '')
         $removeDeployment = $obj.On.workflow_dispatch.inputs.removeDeployment.default
         Write-Warning "Default input value for removeDeployment: $removeDeployment"
 
         # Get 'versioningOption' default input value
-        # $versioningOptionRowIndex = ((0..($content.Count - 1)) | Where-Object { $content[$_] -like '*versioningOption:*' })[0]
-        # $versioningOption = $content[$versioningOptionRowIndex + 3].trim().Split(':')[1].Trim().Replace("'", '').Replace('"', '')
         $versioningOption = $obj.On.workflow_dispatch.inputs.versioningOption.default
         Write-Warning "Default input value for versioningOption: $versioningOption"
 
         # Get 'customVersion' default input value
-        # $customVersionRowIndex = ((0..($content.Count - 1)) | Where-Object { $content[$_] -like '*customVersion:*' })[0]
-        # $customVersion = $content[$customVersionRowIndex + 3].trim().Split(':')[1].Trim().Replace("'", '').Replace('"', '')
         $customVersion = $obj.On.workflow_dispatch.inputs.customVersion.default
         Write-Warning "Default input value for customVersion: $customVersion"
 
