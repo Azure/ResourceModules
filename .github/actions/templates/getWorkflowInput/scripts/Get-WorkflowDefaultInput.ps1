14a186548372686c80b2adf98d17a4cede91ca0e
Marius <Marius.Storhaug@microsoft.com>
2021-10-29 20:33:27 +0200

    Moved helper function into begin

--- .github/actions/templates/getWorkflowInput/scripts/Get-WorkflowDefaultInput.ps1
+++ .github/actions/templates/getWorkflowInput/scripts/Get-WorkflowDefaultInput.ps1
@@ -1,48 +1,5 @@
 <#
 .SYNOPSIS
-Retrieve default value for a specified input in a workflow.
-
-.PARAMETER InputName
-Mandatory. The name of the input to get the default value for.
-
-.PARAMETER Content
-Mandatory. The content of the GitHub workflow file.
-
-.PARAMETER Lines
-Mandatory. The number of lines to check after finding the specified input.
-
-.EXAMPLE
-$content = Get-Content -Path .\workflow.yml
-Get-DefaultValue -Text 'removeDeployment' -Content $Content
-
-Retrieve input default values for the 'removeDeployment' in the workflow.yml file.
-#>
-function Get-DefaultValue {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)]
-        [string] $InputName,
-        [Parameter(Mandatory)]
-        [string[]] $Content,
-        [Parameter()]
-        [int] $Lines = 3
-    )
-    $Content = $Content.Split([Environment]::NewLine)
-    $RowIndex = ((0..($Content.Count - 1)) | Where-Object { $Content[$_] -match "$InputName" })[0]
-    Write-Verbose "Found $InputName on line: $RowIndex"
-    for ($i = $RowIndex; $i -le ($RowIndex+$Lines); $i++) {
-        if ($Content[$i] -match 'default:') {
-            Write-Verbose "Found 'default:' on line: $i"
-            $defaultValue = $Content[$i].trim().Split('#')[0].Split(':')[-1].Replace("'",'').Trim()
-        }
-    }
-    Write-Verbose "Default input value for $InputName`: $defaultValue"
-    return $defaultValue
-}
-
-
-<#
-.SYNOPSIS
 Retrieve input parameter default values for a specified workflow.
 
 .DESCRIPTION
@@ -65,6 +22,50 @@ function Get-WorkflowDefaultInput {
 
     begin {
         Write-Debug ('{0} entered' -f $MyInvocation.MyCommand)
+
+        #region Helper Functions
+        <#
+        .SYNOPSIS
+        Retrieve default value for a specified input in a workflow.
+
+        .PARAMETER InputName
+        Mandatory. The name of the input to get the default value for.
+
+        .PARAMETER Content
+        Mandatory. The content of the GitHub workflow file.
+
+        .PARAMETER Lines
+        Mandatory. The number of lines to check after finding the specified input.
+
+        .EXAMPLE
+        $content = Get-Content -Path .\workflow.yml
+        Get-DefaultValue -Text 'removeDeployment' -Content $Content
+
+        Retrieve input default values for the 'removeDeployment' in the workflow.yml file.
+        #>
+        function Get-DefaultValue {
+            [CmdletBinding()]
+            param (
+                [Parameter(Mandatory)]
+                [string] $InputName,
+                [Parameter(Mandatory)]
+                [string[]] $Content,
+                [Parameter()]
+                [int] $Lines = 3
+            )
+            $Content = $Content.Split([Environment]::NewLine)
+            $RowIndex = ((0..($Content.Count - 1)) | Where-Object { $Content[$_] -match "$InputName" })[0]
+            Write-Verbose "Found $InputName on line: $RowIndex"
+            for ($i = $RowIndex; $i -le ($RowIndex+$Lines); $i++) {
+                if ($Content[$i] -match 'default:') {
+                    Write-Verbose "Found 'default:' on line: $i"
+                    $defaultValue = $Content[$i].trim().Split('#')[0].Split(':')[-1].Replace("'",'').Trim()
+                }
+            }
+            Write-Verbose "Default input value for $InputName`: $defaultValue"
+            return $defaultValue
+        }
+        #endregion
     }
 
     process {
