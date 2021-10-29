001f53fc42e5538f20146eef6b83c8352dd34c1b
Marius <Marius.Storhaug@microsoft.com>
2021-10-29 17:23:22 +0200

    Testing env + yml parser

--- .github/actions/templates/getWorkflowInput/scripts/Get-WorkflowDefaultInput.ps1
+++ .github/actions/templates/getWorkflowInput/scripts/Get-WorkflowDefaultInput.ps1
@@ -22,11 +22,11 @@ function Get-WorkflowDefaultInput {
 
     begin {
         Write-Debug ('{0} entered' -f $MyInvocation.MyCommand)
-        Install-Module -Name powershell-yaml -Repository PSGallery -Force
-        Import-Module -Name powershell-yaml -Verbose
     }
 
     process {
+        Install-Module -Name powershell-yaml -Repository PSGallery -Force -Verbose
+        Import-Module -Name powershell-yaml -Verbose
         #$content = Get-Content $workflowPath
         $content = Get-Content $workflowPath -Raw
         $obj = ConvertFrom-Yaml -Yaml $content
@@ -35,26 +35,26 @@ function Get-WorkflowDefaultInput {
         # $removeDeploymentRowIndex = ((0..($content.Count - 1)) | Where-Object { $content[$_] -like '*removeDeployment:*' })[0]
         # $removeDeployment = $content[$removeDeploymentRowIndex + 3].trim().Split(':')[1].Trim().Replace("'", '').Replace('"', '')
         $removeDeployment = $obj.On.workflow_dispatch.inputs.removeDeployment.default
-        Write-Verbose "Default input value for removeDeployment: $removeDeployment"
+        Write-Warning "Default input value for removeDeployment: $removeDeployment"
 
         # Get 'versioningOption' default input value
         # $versioningOptionRowIndex = ((0..($content.Count - 1)) | Where-Object { $content[$_] -like '*versioningOption:*' })[0]
         # $versioningOption = $content[$versioningOptionRowIndex + 3].trim().Split(':')[1].Trim().Replace("'", '').Replace('"', '')
         $versioningOption = $obj.On.workflow_dispatch.inputs.versioningOption.default
-        Write-Verbose "Default input value for versioningOption: $versioningOption"
+        Write-Warning "Default input value for versioningOption: $versioningOption"
 
         # Get 'customVersion' default input value
         # $customVersionRowIndex = ((0..($content.Count - 1)) | Where-Object { $content[$_] -like '*customVersion:*' })[0]
         # $customVersion = $content[$customVersionRowIndex + 3].trim().Split(':')[1].Trim().Replace("'", '').Replace('"', '')
         $customVersion = $obj.On.workflow_dispatch.inputs.customVersion.default
-        Write-Verbose "Default input value for customVersion: $customVersion"
+        Write-Warning "Default input value for customVersion: $customVersion"
 
         # Define hashtable to contain workflow parameters
         $workflowParameters = @{}
         $workflowParameters.Add('removeDeployment', $removeDeployment)
         $workflowParameters.Add('versioningOption', $versioningOption)
         $workflowParameters.Add('customVersion', $customVersion)
-        Write-Verbose 'Get workflow default input complete'
+        Write-Warning 'Get workflow default input complete'
 
         # Return hashtable
         return $workflowParameters
