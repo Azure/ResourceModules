4127217d995c35bca839091853d73cb13892aea1
MrMCake <asehr@hotmail.de>
2021-10-31 16:44:03 +0100

    Update to latest

---
+++ .github/actions/templates/publishModule/scripts/Publish-ModuleToPrivateBicepRegistry.ps1
@@ -0,0 +1,142 @@
+<#
+.SYNOPSIS
+Publish a new version of a given module to a private bicep registry
+
+.DESCRIPTION
+Publish a new version of a given module to a private bicep registry
+The function will take evaluate which version should be published based on the provided input parameters (customVersion, versioningOption) and the version currently deployed to the private bicep registry
+If the customVersion is higher than the current latest, it has the highest priority over the other options
+Otherwise, one of the provided version options is chosen and applied with the default being 'patch'
+
+.PARAMETER componentTemplateSpecName
+Mandatory. The name of the module to publish. It will be the name of the private bicep registry.
+
+.PARAMETER templateFilePath
+Mandatory. Path to the module deployment file from root.
+
+.PARAMETER componentBicepRegistryName
+Mandatory. Name of the private bicep registry to publish to.
+
+.PARAMETER componentBicepRegistryRGName
+Mandatory. ResourceGroup of the private bicep registry to publish to.
+
+.PARAMETER customVersion
+Optional. A custom version that can be provided by the UI. '-' represents an empty value.
+
+.PARAMETER versioningOption
+Optional. A version option that can be specified in the UI. Defaults to 'patch'
+
+.EXAMPLE
+Publish-ModuleToPrivateBicepRegistry -componentTemplateSpecName 'KeyVault' templateFilePath 'C:/KeyVault/deploy.json' -componentBicepRegistryRGName 'artifacts-rg' -componentTemplateSpecDescription 'iacs key vault' -customVersion '3.0.0'
+
+Try to publish the KeyVault module with version 3.0.0 to a private bicep registry called KeyVault based on a value provided in the UI
+#>
+function Publish-ModuleToPrivateBicepRegistry {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory)]
+        [string] $componentTemplateSpecName,
+
+        [Parameter(Mandatory)]
+        [string] $templateFilePath,
+
+        [Parameter(Mandatory)]
+        [string] $componentBicepRegistryRGName,
+
+        [Parameter(Mandatory)]
+        [string] $componentBicepRegistryName,
+
+        [Parameter(Mandatory)]
+        [string] $componentTemplateSpecDescription,
+
+        [Parameter(Mandatory = $false)]
+        [string] $customVersion = '0.0.1',
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('Major', 'Minor', 'Patch')]
+        [string] $versioningOption = 'Patch'
+    )
+
+    begin {
+        Write-Debug ('{0} entered' -f $MyInvocation.MyCommand)
+    }
+
+    process {
+        #############################
+        ##    EVALUATE RESOURCES   ##
+        #############################
+        if (-not (Get-AzContainerRegistry -ResourceGroupName $componentBicepRegistryRGName -Name $componentBicepRegistryName -ErrorAction 'SilentlyContinue')) {
+            if ($PSCmdlet.ShouldProcess("Container Registry [$componentBicepRegistryName] to resource group [$componentBicepRegistryRGName]", 'Deploy')) {
+                New-AzContainerRegistry -ResourceGroupName $componentBicepRegistryRGName -Name $componentBicepRegistryName -Sku 'Basic'
+            }
+        }
+
+        #################################
+        ##    FIND AVAILABLE VERSION   ##
+        #################################
+        if ($PSCmdlet.ShouldProcess("Latest available version in private bicep registry [$componentTemplateSpecName]", 'Fetch')) {
+            $res = Get-AzTemplateSpec -ResourceGroupName $componentBicepRegistryRGName -Name $componentTemplateSpecName -ErrorAction 'SilentlyContinue'
+        }
+        if (-not $res) {
+            Write-Verbose "No version detected in private bicep registry [$componentTemplateSpecName]. Creating new."
+            $latestVersion = New-Object System.Version('0.0.0')
+        } else {
+            $uniqueVersions = $res.Versions.Name | Get-Unique | Where-Object { $_ -like '*.*.*' } # remove Where-object for working example
+            $latestVersion = (($uniqueVersions -as [Version[]]) | Measure-Object -Maximum).Maximum
+            Write-Verbose "Published versions detected in private bicep registry [$componentTemplateSpecName]. Fetched latest [$latestVersion]."
+        }
+
+        ############################
+        ##    EVALUATE VERSION    ##
+        ############################
+
+        if (-not ([String]::IsNullOrEmpty($customVersion)) -and ((New-Object System.Version($customVersion)) -gt (New-Object System.Version($latestVersion)))) {
+            Write-Verbose "A custom version [$customVersion] was specified in the pipeline script and is higher than the current latest. Using it."
+            $newVersion = $customVersion
+        } else {
+            Write-Verbose 'No custom version set. Using default versioning.'
+
+            switch ($versioningOption) {
+                'major' {
+                    Write-Verbose 'Apply version update on "major" level'
+                    $newVersion = (New-Object -TypeName System.Version -ArgumentList ($latestVersion.Major + 1), $latestVersion.Minor, $latestVersion.Build).ToString()
+                    break
+                }
+                'minor' {
+                    Write-Verbose 'Apply version update on "minor" level'
+                    $newVersion = (New-Object -TypeName System.Version -ArgumentList $latestVersion.Major, ($latestVersion.Minor + 1), $latestVersion.Build).ToString()
+                    break
+                }
+                'patch' {
+                    Write-Verbose 'Apply version update on "patch" level'
+                    $newVersion = (New-Object -TypeName System.Version -ArgumentList $latestVersion.Major, $latestVersion.Minor, ($latestVersion.Build + 1)).ToString()
+                    break
+                }
+                default {
+                    throw "Unsupported version option: $versioningOption."
+                }
+            }
+        }
+
+        $newVersionObject = New-Object System.Version($newVersion)
+        if ($newVersionObject -lt $latestVersion -or $newVersionObject -eq $latestVersion) {
+            throw ('The provided custom version [{0}] must be higher than the current latest version [{1}] published in the private bicep registry [{2}]' -f $newVersionObject.ToString(), $latestVersion.ToString(), $componentTemplateSpecName)
+        }
+
+        ################################
+        ##    Create private bicep registry    ##
+        ################################
+        if ($PSCmdlet.ShouldProcess("private bicep registry [$componentTemplateSpecName] version [$newVersion]", 'Publish')) {
+            $templateSpecInputObject = @{
+
+            }
+            New-AzTemplateSpec @templateSpecInputObject
+        }
+        Write-Verbose 'Publish complete'
+    }
+
+    end {
+        Write-Debug ('{0} exited' -f $MyInvocation.MyCommand)
+    }
+}
