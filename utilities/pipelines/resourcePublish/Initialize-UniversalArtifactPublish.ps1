a95eb24646043f571af39aea182b0180042bc3a7
MrMCake <asehr@hotmail.de>
2021-11-14 18:42:30 +0200

    Update to latest

--- utilities/pipelines/resourcePublish/Initialize-UniversalArtifactPublish.ps1
+++ utilities/pipelines/resourcePublish/Initialize-UniversalArtifactPublish.ps1
@@ -9,12 +9,12 @@ The customVersion is considered only if it is higher than the latest version dep
 Otherwise, one of the provided version options is chosen and applied with the default being 'patch'
 
 The function returns
-- the provided module name as a lowercase version (required by the publish-task)
+- the name of the artifact to publish
 - the version option to be applied if applicable
 - the version to be applied if applicable
 
-.PARAMETER moduleName
-Mandatory. The name of the module to publish.
+.PARAMETER templateFilePath
+Mandatory. Path to the module deployment file from root.
 
 .PARAMETER vstsOrganization
 Mandatory. Name of the organization hosting the artifacts feed.
@@ -32,17 +32,17 @@ Optional. A custom version the can be provided as a value in the pipeline file.
 Optional. A version option that can be specified in the UI. Defaults to 'patch'
 
 .EXAMPLE
-Initialize-UniversalArtifactPublish -moduleName 'keyvault' -vstsOrganization 'servicescode' -vstsProject '$(System.TeamProject)' -vstsFeedName 'Modules' -customVersion '3.0.0'
+Initialize-UniversalArtifactPublish -templateFilePath 'C:/KeyVault/deploy.json' -vstsOrganization 'servicescode' -vstsProject '$(System.TeamProject)' -vstsFeedName 'Modules' -customVersion '3.0.0'
 
 Try to publish the key vault module with version 3.0.0 to the module feed 'servicescode/$(System.TeamProject)/Modules' based on a value provided in the UI
 
 .EXAMPLE
-Initialize-UniversalArtifactPublish -moduleName 'keyvault' -vstsOrganization 'servicescode' -vstsProject '$(System.TeamProject)' -vstsFeedName 'Modules' -customVersion '1.0.0'
+Initialize-UniversalArtifactPublish -templateFilePath 'C:/KeyVault/deploy.json' -vstsOrganization 'servicescode' -vstsProject '$(System.TeamProject)' -vstsFeedName 'Modules' -customVersion '1.0.0'
 
 Try to publish the key vault module with version 1.0.0 to the module feed 'servicescode/$(System.TeamProject)/Modules' based on a value provided in the pipeline file
 
 .EXAMPLE
-Initialize-UniversalArtifactPublish -moduleName 'keyvault' -vstsOrganization 'servicescode' -vstsProject '$(System.TeamProject)' -vstsFeedName 'Modules'
+Initialize-UniversalArtifactPublish -templateFilePath 'C:/KeyVault/deploy.json' -vstsOrganization 'servicescode' -vstsProject '$(System.TeamProject)' -vstsFeedName 'Modules'
 
 Try to publish the next key vault module version to the module feed 'servicescode/$(System.TeamProject)/Modules' based on the default versioning behavior
 #>
@@ -51,7 +51,7 @@ function Initialize-UniversalArtifactPublish {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
-        [string] $moduleName,
+        [string] $templateFilePath,
 
         [Parameter(Mandatory)]
         [string] $vstsOrganization,
@@ -78,24 +78,25 @@ function Initialize-UniversalArtifactPublish {
         # For function output
         $resultSet = @{}
 
-        $lowerModuleName = $moduleName.ToLower()
-        $resultSet['lowerModuleName'] = $lowerModuleName
+        $moduleIdentifier = (Split-Path $templateFilePath -Parent).Replace('\', '/').Split('/arm/')[1]
+        $universalPackageModuleName = $moduleIdentifier.Replace('\', '/').Replace('/', '.').ToLower()
+        $resultSet['universalPackageModuleName'] = $universalPackageModuleName
 
         #################################
         ##    FIND AVAILABLE VERSION   ##
         #################################
         try {
             $head = @{ Authorization = "Bearer $env:TOKEN" }
-            $url = "https://feeds.dev.azure.com/$vstsOrganization/$vstsProject/_apis/packaging/Feeds/$vstsFeedName/packages?packageNameQuery=$lowerModuleName&api-version=6.0-preview"
+            $url = "https://feeds.dev.azure.com/$vstsOrganization/$vstsProject/_apis/packaging/Feeds/$vstsFeedName/packages?packageNameQuery=$universalPackageModuleName&api-version=6.0-preview"
             $packages = Invoke-RestMethod -Uri $url -Method Get -Headers $head -ContentType application/json
             if ($packages) {
                 if ($packages.value.id.count -gt 1) {
                     # Handle the case where multiple modules in the feed start with with the same packageName. In this case we have to filter the result of the REST query even further.
-                    $packages.value = $packages.value | Where-Object { $_.Name -eq $lowerModuleName }
+                    $packages.value = $packages.value | Where-Object { $_.Name -eq $universalPackageModuleName }
                 }
 
                 $latestFeedVersion = ($packages.value.versions.Where( { $_.isLatest -eq $True })).version
-                Write-Verbose ('Package Id of [{0}] is [{1}]' -f $lowerModuleName, $packages.value.Id) -Verbose
+                Write-Verbose ('Package Id of [{0}] is [{1}]' -f $universalPackageModuleName, $packages.value.Id) -Verbose
                 Write-Verbose "The latest version is [$latestFeedVersion]" -Verbose
             } else {
                 Write-Verbose "No packages via url [$url] found" -Verbose
@@ -109,7 +110,7 @@ function Initialize-UniversalArtifactPublish {
         ############################
 
         if ([String]::IsNullOrEmpty($latestFeedVersion)) {
-            Write-Verbose ('No version for module [{0}] found in feed [{1}]. Assuming intial publish' -f $lowerModuleName, $vstsFeedName) -Verbose
+            Write-Verbose ('No version for module [{0}] found in feed [{1}]. Assuming intial publish' -f $universalPackageModuleName, $vstsFeedName) -Verbose
             $latestFeedVersion = New-Object System.Version('0.0.1')
         } elseif (-not (([String]::IsNullOrEmpty($customVersion)) -or ([String]::IsNullOrEmpty($latestFeedVersion))) -and ((New-Object System.Version($customVersion)) -gt (New-Object System.Version($latestFeedVersion))) ) {
             Write-Verbose "A custom version [$customVersion] was specified in the pipeline script and is higher than the current latest. Using it." -Verbose
