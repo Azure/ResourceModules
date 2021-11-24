1402e4665e8f6357681f8849fb475eb833fac31e
MrMCake <asehr@hotmail.de>
2021-11-24 14:13:15 +0100

    Simpliefied vwan removal script

--- utilities/pipelines/resourceRemoval/Remove-vWan.ps1
+++ utilities/pipelines/resourceRemoval/Remove-vWan.ps1
@@ -61,61 +61,20 @@ function Remove-vWan {
         }
 
         $resourcesToRemove = @()
-        $virtualWANsToRemove = $deployment.TargetResource | Where-Object { $_ -and $_ -notmatch '/deployments/' }
+        $unorderedResourceIds = $deployment.TargetResource | Where-Object { $_ -and $_ -notmatch '/deployments/' }
 
-        $resourcesToRemove = @()
-        foreach ($vWANResourceId in $virtualWANsToRemove) {
-
-            $virtualWANInstance = Get-AzResource -ResourceId $vWANResourceId
-
-            $resourcesToRemove += @{
-                resourceId = $virtualWANInstance.ResourceId
-                name       = $virtualWANInstance.Name
-                type       = $virtualWANInstance.Type
-            }
-
-            $vpnSite = Get-AzVpnSite -ResourceGroupName $ResourceGroupName | Where-Object { $_.VirtualWan.Id -eq $virtualWANInstance.ResourceId }
-            Write-Verbose ('Found [{0}] vpnSite(s) in virtual WAN [{1}]' -f $vpnSite.Count, $virtualWANInstance.name)
-
-            foreach ($vpnSiteInstance in $vpnSite) {
-                $resourcesToRemove += @{
-                    resourceId = $vpnSiteInstance.Id
-                    name       = $vpnSiteInstance.Name
-                    type       = $vpnSiteInstance.Type
-                }
-            }
-
-            $virtualHub = Get-AzVirtualHub -ResourceGroupName $ResourceGroupName | Where-Object { $_.VirtualWan.Id -eq $virtualWANInstance.ResourceId }
-            Write-Verbose ('Found [{0}] virtual Hub(s) in virtual WAN [{1}]' -f $virtualHub.Count, $virtualWANInstance.name)
-
-            foreach ($virtualHubInstance in $virtualHub) {
-                $resourcesToRemove += @{
-                    resourceId = $virtualHub.Id
-                    name       = $virtualHub.Name
-                    type       = $virtualHub.Type
-                }
-
-                $vpnGateway = Get-AzVpnGateway -ResourceGroupName $ResourceGroupName | Where-Object { $_.VirtualHub.Id -eq $virtualHubInstance.Id }
-                Write-Verbose ('Found [{0}] vpnGateway(s) in virtual virtual Hub [{1}]' -f $vpnGateway.Count, $virtualHubInstance.name)
-
-                foreach ($vpnGatewayInstance in $vpnGateway) {
-                    $resourcesToRemove += @{
-                        resourceId = $vpnGatewayInstance.Id
-                        name       = $vpnGatewayInstance.Name
-                        type       = $vpnGatewayInstance.Type
-                    }
-                }
+        $orderedResourceIds = @(
+            $unorderedResourceIds | Where-Object { $_ -match 'Microsoft.Network/vpnGateways' }
+            $unorderedResourceIds | Where-Object { $_ -match 'Microsoft.Network/virtualHubs' }
+            $unorderedResourceIds | Where-Object { $_ -match 'Microsoft.Network/vpnSites' }
+            $unorderedResourceIds | Where-Object { $_ -match 'Microsoft.Network/virtualWans' }
+        )
+        $resourcesToRemove = $orderedResourceIds | ForEach-Object {
+            @{ resourceId = $_
+                name      = $_.Split('/')[-1]
+                type      = $_.Split('/')[6..7] -join '/'
             }
         }
-
-        # Prepare resources
-        # -----------------
-        # Flip array to remove low-level resources first
-        $invertedArray = @()
-        for ($i = ($resourcesToRemove.count - 1); $i -ge 0; $i--) {
-            $invertedArray += $resourcesToRemove[$i]
-        }
-        $resourcesToRemove = $invertedArray
 
         # Remove resources
         # ----------------
