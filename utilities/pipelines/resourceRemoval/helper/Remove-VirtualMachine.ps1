0e887b7a50fac8f29f81966ae1f39132a6404330
MrMCake <asehr@hotmail.de>
2021-11-30 12:49:41 +0100

    Update to latest

--- utilities/pipelines/resourceRemoval/helper/Remove-VirtualMachine.ps1
+++ utilities/pipelines/resourceRemoval/helper/Remove-VirtualMachine.ps1
@@ -63,18 +63,35 @@ function Remove-VirtualMachine {
         }
 
         $unorderedResourceIds = $deployments.TargetResource | Where-Object { $_ -and $_ -notmatch '/deployments/' }
-        $vmName = ($unorderedResourceIds | Where-Object { $_ -match '/virtualMachines/' }).Split('/')[-1]
+        $resourcesToRemove = [System.Collections.ArrayList] @()
 
-        # Fetch all resources that match the VM
-        $allResources = Get-AzResource -ResourceGroupName $resourceGroupName -Name "$vmName*"
 
-        # Sort ascending as we need to remove the VM first
-        $orderedResourceIds = $allResources | Sort-Object -Property { $_.ResourceId.Split('/').Count }
-        $resourcesToRemove = $orderedResourceIds | ForEach-Object {
-            @{
-                resourceId = $_.ResourceId
-                name       = $_.Name
-                type       = $_.Type
+        # Handle VM resources
+        $vmIds = $unorderedResourceIds | Where-Object { $_ -match '/virtualMachines/' }
+        if ($vmIds.Count -gt 0) {
+            $vmName = ($unorderedResourceIds | Where-Object { $_ -match '/virtualMachines/' }).Split('/')[-1]
+
+            # Fetch all resources that match the VM
+            $allResources = Get-AzResource -ResourceGroupName $resourceGroupName -Name "$vmName*"
+
+            # Sort ascending as we need to remove the VM first
+            $orderedResourceIds = $allResources | Sort-Object -Property { $_.ResourceId.Split('/').Count }
+            $resourcesToRemove += $orderedResourceIds | ForEach-Object {
+                @{
+                    resourceId = $_.ResourceId
+                    name       = $_.Name
+                    type       = $_.Type
+                }
+            }
+        }
+
+        # Handle non-vm resources that are deployed with the VM
+        $otherIds = $unorderedResourceIds | Where-Object { $_ -notmatch '/virtualMachines/' }
+        foreach ($otherResourceId in $otherIds) {
+            $resourcesToRemove += @{
+                resourceId = $otherResourceId
+                name       = $otherResourceId.Split('/')[-1]
+                type       = $otherResourceId.Split('/')[6..7] -join '/'
             }
         }
 
