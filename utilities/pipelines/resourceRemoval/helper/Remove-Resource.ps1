3a055c6fb112a662a16f9d9654bf5b0b924087c7
MrMCake <asehr@hotmail.de>
2021-12-07 01:54:59 +0100

    Update to latest

--- utilities/pipelines/resourceRemoval/helper/Remove-Resource.ps1
+++ utilities/pipelines/resourceRemoval/helper/Remove-Resource.ps1
@@ -93,22 +93,21 @@ function Initialize-PreResourceRemoval {
     switch ($resourceToRemove.type) {
         'Microsoft.RecoveryServices/vaults' {
             # Remove protected VMs
-
             if ((Get-AzRecoveryServicesVaultProperty -VaultId $resourceToRemove.resourceId).SoftDeleteFeatureState -ne 'Disabled') {
-                if ($PSCmdlet.ShouldProcess(('Soft-delete on RSV [{0}]' -f $resourceToRemove.name), 'Set')) {
+                if ($PSCmdlet.ShouldProcess(('Soft-delete on RSV [{0}]' -f $resourceToRemove.resourceId), 'Set')) {
                     $null = Set-AzRecoveryServicesVaultProperty -VaultId $resourceToRemove.resourceId -SoftDeleteFeatureState 'Disable'
                 }
             }
 
             $backupItems = Get-AzRecoveryServicesBackupItem -BackupManagementType 'AzureVM' -WorkloadType 'AzureVM' -VaultId $resourceToRemove.resourceId
             foreach ($backupItem in $backupItems) {
-                Write-Verbose ('Removing Backup item [{0}] from RSV [{1}]' -f $backupItem.Name, $resourceToRemove.name) -Verbose
+                Write-Verbose ('Removing Backup item [{0}] from RSV [{1}]' -f $backupItem.Name, $resourceToRemove.resourceId) -Verbose
 
                 if ($PSCmdlet.ShouldProcess('Soft-delete backup data removal', 'Undo')) {
                     $null = Undo-AzRecoveryServicesBackupItemDeletion -Item $backupItem -VaultId $resourceToRemove.resourceId -Force
                 }
 
-                if ($PSCmdlet.ShouldProcess(('Backup item [{0}] from RSV [{1}]' -f $backupItem.Name, $resourceToRemove.name), 'Remove')) {
+                if ($PSCmdlet.ShouldProcess(('Backup item [{0}] from RSV [{1}]' -f $backupItem.Name, $resourceToRemove.resourceId), 'Remove')) {
                     $null = Disable-AzRecoveryServicesBackupProtection -Item $backupItem -VaultId $resourceToRemove.resourceId -RemoveRecoveryPoints -Force
                 }
             }
