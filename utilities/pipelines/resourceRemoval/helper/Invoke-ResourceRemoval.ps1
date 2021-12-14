bb9dccf14d0febb11ffddb49e81deb84ef3ccd7c
Erika Gressi <erikag.microsoft.com>
2021-12-14 01:31:05 +0100

    invoke removal docs

--- utilities/pipelines/resourceRemoval/helper/Invoke-ResourceRemoval.ps1
+++ utilities/pipelines/resourceRemoval/helper/Invoke-ResourceRemoval.ps1
@@ -5,33 +5,21 @@ Remove a specific resource
 .DESCRIPTION
 Remove a specific resource. Tries to handle different resource types accordingly
 
-.PARAMETER resourceId
-Mandatory. The resourceID of the resource to remove
-
-.PARAMETER name
-Mandatory. The name of the resource to remove
-
-.PARAMETER type
-Mandatory. The type of the resource to remove
+.PARAMETER ResourceToRemove
+Mandatory. The resource to remove. Should have format
+@{
+    name        = '...'
+    resourceID = '...'
+    type        = '...'
+}
 
 .EXAMPLE
-Invoke-ResourceRemoval -name 'sxx-vm-linux-001-nic-01-diagnosticSettings' -type 'Microsoft.Insights/diagnosticSettings' -resourceId '/subscriptions/a7439831-1cd9-435d-a091-4aa863c96556/resourceGroups/validation-rg/providers/Microsoft.Network/networkInterfaces/sxx-vm-linux-001-nic-01/providers/Microsoft.Insights/diagnosticSettings/sxx-vm-linux-001-nic-01-diagnosticSettings'
+Invoke-ResourcePostRemoval -ResourceToRemove @{ name = 'sxx-vm-linux-001-nic-01-diagnosticSettings'; resourceId '(..)/Microsoft.Network/networkInterfaces/sxx-vm-linux-001-nic-01/providers/Microsoft.Insights/diagnosticSettings/sxx-vm-linux-001-nic-01-diagnosticSettings'; type = 'Microsoft.Insights/diagnosticSettings'}
 
 Remove the resource 'sxx-vm-linux-001-nic-01-diagnosticSettings' of type 'Microsoft.Insights/diagnosticSettings' from resource '/subscriptions/a7439831-1cd9-435d-a091-4aa863c96556/resourceGroups/validation-rg/providers/Microsoft.Network/networkInterfaces/sxx-vm-linux-001-nic-01'
 #>
 function Invoke-ResourceRemoval {
 
-    # [CmdletBinding(SupportsShouldProcess)]
-    # param (
-    #     [Parameter()]
-    #     [string] $resourceId,
-
-    #     [Parameter()]
-    #     [string] $name,
-
-    #     [Parameter()]
-    #     [string] $type
-    # )
     [CmdletBinding(SupportsShouldProcess)]
     param (
         [Parameter(Mandatory)]
