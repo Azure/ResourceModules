cabea2ecd9c8416eac047278848684e4d5fbabc6
Rainer Halanek <rahalan@microsoft.com>
2021-12-16 10:44:42 +0100

    minor changes

--- utilities/tools/ConvertTo-ARMTemplate.Tests.ps1
+++ utilities/tools/ConvertTo-ARMTemplate.Tests.ps1
@@ -1,17 +1,20 @@
 ﻿Describe 'Convert bicep files to ARM' {
     BeforeAll {
-        $bicepFilesCount = (Get-ChildItem -Recurse '.\' | Where-Object { $_.FullName -match 'deploy.bicep' }).Count
-        # run converion script
-        #.\ConvertTo-ARMTemplate.ps1
+        $deployBicepFilesCount = (Get-ChildItem -Recurse '.\' | Where-Object { $_.FullName -match 'deploy.bicep' }).Count
+        $nestedBicepFilesCount = (Get-ChildItem -Recurse '.\' | Where-Object { $_.FullName -match 'nested*.bicep' }).Count
+        Write-Verbose "$deployBicepFilesCount deploy.bicep file(s) found"
+        Write-Verbose "$nestedBicepFilesCount nested bicep file(s) found"
+        Write-Verbose 'run ConvertTo-ARMTemplate script'
+        .\ConvertTo-ARMTemplate.ps1 -Path '.\..\..\'
     }
 
-    It 'all bicep files are converted to ARM' {
+    It 'all deploy bicep files are converted to ARM' {
         $jsonFilesCount = (Get-ChildItem -Recurse '.\' | Where-Object { $_.FullName -match 'deploy.json' }).Count
-        $bicepFilesCount | Should -Be $jsonFilesCount
+        $deployBicepFilesCount | Should -Be $jsonFilesCount
     }
 
-    It 'all bicep files are converted to ARM' {
-        $jsonFilesCount = (Get-ChildItem -Recurse '.\' | Where-Object { $_.FullName -match 'deploy.json' }).Count
-        $bicepFilesCount | Should -Be $jsonFilesCount
+    It 'all nested bicep files are removed' {
+        $nestedBicepFilesCount = (Get-ChildItem -Recurse '.\' | Where-Object { $_.FullName -match 'nested*.bicep' }).Count
+        $nestedBicepFilesCount | Should -Be 0
     }
 }
