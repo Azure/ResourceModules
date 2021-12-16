c030ecec9fe521005849bd65de77e2d7e994a635
Rainer Halanek <rahalan@microsoft.com>
2021-12-16 12:16:12 +0100

    update paths

--- utilities/tools/ConvertTo-ARMTemplate.Tests.ps1
+++ utilities/tools/ConvertTo-ARMTemplate.Tests.ps1
@@ -1,24 +1,28 @@
 ﻿Describe 'Convert bicep files to ARM' {
     BeforeAll {
-        $repoRootPath = '.\..\..\'
-        $modulesPath = '.\..\..\arm\'
-        $deployBicepFilesCount = (Get-ChildItem -Recurse $modulesPath | Where-Object { $_.FullName -match 'deploy.bicep' }).Count
-        $nestedBicepFilesCount = (Get-ChildItem -Recurse $modulesPath | Where-Object { $_.FullName -match 'nested_.*bicep' }).Count
+        $rootPath = Get-Item -Path $Path | Select-Object -ExpandProperty 'FullName'
+        $armFolderPath = Join-Path -Path $rootPath -ChildPath 'arm'
+
+        $deployBicepFilesCount = (Get-ChildItem -Recurse $armFolderPath | Where-Object { $_.FullName -match 'deploy.bicep' }).Count
+        $nestedBicepFilesCount = (Get-ChildItem -Recurse $armFolderPath | Where-Object { $_.FullName -match 'nested_.*bicep' }).Count
         Write-Host "$deployBicepFilesCount deploy.bicep file(s) found"
         Write-Host "$nestedBicepFilesCount nested bicep file(s) found"
         Write-Host 'run ConvertTo-ARMTemplate script'
-        .\ConvertTo-ARMTemplate.ps1 -Path $repoRootPath
+        .\ConvertTo-ARMTemplate.ps1 -Path $rootPath
     }
 
     It 'all deploy.bicep files are converted to deploy.json' {
-        $deployJsonFilesCount = (Get-ChildItem -Recurse $modulesPath | Where-Object { $_.FullName -match 'deploy.json' }).Count
+        $deployJsonFilesCount = (Get-ChildItem -Recurse $armFolderPath | Where-Object { $_.FullName -match 'deploy.json' }).Count
         Write-Host "$deployJsonFilesCount deploy.json file(s) found"
         $deployJsonFilesCount | Should -Be $deployBicepFilesCount
     }
 
     It 'all bicep files are removed' {
-        $bicepFilesCount = (Get-ChildItem -Recurse $modulesPath | Where-Object { $_.FullName -match '.*.bicep' }).Count
+        $bicepFilesCount = (Get-ChildItem -Recurse $armFolderPath | Where-Object { $_.FullName -match '.*.bicep' }).Count
         Write-Host "$bicepFilesCount bicep file(s) found"
         $bicepFilesCount | Should -Be 0
     }
+
+    # $workflowFolderPath = Join-Path -Path $rootPath -ChildPath '.github\workflows'
+    # $workflowFilesToUpdate = Get-ChildItem -Path $workflowFolderPath -Filter 'ms.*.yml' -File -Force
 }
