74f0bf0f7f9e82f1fc112346170400f325e5b181
Rainer Halanek <rahalan@microsoft.com>
2021-12-16 10:52:07 +0100

    fix path

--- utilities/tools/ConvertTo-ARMTemplate.Tests.ps1
+++ utilities/tools/ConvertTo-ARMTemplate.Tests.ps1
@@ -1,20 +1,23 @@
 ﻿Describe 'Convert bicep files to ARM' {
     BeforeAll {
-        $deployBicepFilesCount = (Get-ChildItem -Recurse '.\' | Where-Object { $_.FullName -match 'deploy.bicep' }).Count
-        $nestedBicepFilesCount = (Get-ChildItem -Recurse '.\' | Where-Object { $_.FullName -match 'nested*.bicep' }).Count
-        Write-Verbose "$deployBicepFilesCount deploy.bicep file(s) found"
-        Write-Verbose "$nestedBicepFilesCount nested bicep file(s) found"
-        Write-Verbose 'run ConvertTo-ARMTemplate script'
-        .\ConvertTo-ARMTemplate.ps1 -Path '.\..\..\'
+        $repoRootPath = '.\..\..\'
+        $deployBicepFilesCount = (Get-ChildItem -Recurse $repoRootPath | Where-Object { $_.FullName -match 'deploy.bicep' }).Count
+        $nestedBicepFilesCount = (Get-ChildItem -Recurse $repoRootPath | Where-Object { $_.FullName -match 'nested*.bicep' }).Count
+        Write-Host "$deployBicepFilesCount deploy.bicep file(s) found"
+        Write-Host "$nestedBicepFilesCount nested bicep file(s) found"
+        Write-Host 'run ConvertTo-ARMTemplate script'
+        .\ConvertTo-ARMTemplate.ps1 -Path $repoRootPath
     }
 
     It 'all deploy bicep files are converted to ARM' {
-        $jsonFilesCount = (Get-ChildItem -Recurse '.\' | Where-Object { $_.FullName -match 'deploy.json' }).Count
-        $deployBicepFilesCount | Should -Be $jsonFilesCount
+        $deployJsonFilesCount = (Get-ChildItem -Recurse $repoRootPath | Where-Object { $_.FullName -match 'deploy.json' }).Count
+        Write-Host "$deployJsonFilesCount deploy.json file(s) found"
+        $deployBicepFilesCount | Should -Be $deployJsonFilesCount
     }
 
     It 'all nested bicep files are removed' {
-        $nestedBicepFilesCount = (Get-ChildItem -Recurse '.\' | Where-Object { $_.FullName -match 'nested*.bicep' }).Count
+        $nestedBicepFilesCount = (Get-ChildItem -Recurse $repoRootPath | Where-Object { $_.FullName -match 'nested*.bicep' }).Count
+        Write-Host "$nestedBicepFilesCount nested bicep file(s) found"
         $nestedBicepFilesCount | Should -Be 0
     }
 }
