a9fe2fdec3ec8f22026d1a2af33027cb2ea12e4c
Erika Gressi <erikag.microsoft.com>
2021-12-29 13:50:38 +0100

    dependencies q

--- utilities/pipelines/resourceRemoval/helper/Get-ResourceIdsAsFormattedObjectList.ps1
+++ utilities/pipelines/resourceRemoval/helper/Get-ResourceIdsAsFormattedObjectList.ps1
@@ -9,12 +9,12 @@ Format the provide resource IDs into objects of resourceID, name & type
 Optional. The resource IDs to process
 
 .EXAMPLE
-Get-ResourceIdsAsFormattedObjectList -ResourceIds @('/subscriptions/<subscriptionID>/resourceGroups/test-analysisServices-parameters.json-rg/providers/Microsoft.Storage/storageAccounts/adpsxxazsaaspar01')
+Get-ResourceIdsAsFormattedObjectList -ResourceIds @('/subscriptions/<subscriptionID>/resourceGroups/test-analysisServices-parameters.json-rg/providers/Microsoft.Storage/storageAccounts/adpqsxxazsaaspar01')
 
 Returns an object @{
-    resourceId = '/subscriptions/<subscriptionID>/resourceGroups/test-analysisServices-parameters.json-rg/providers/Microsoft.Storage/storageAccounts/adpsxxazsaaspar01'
+    resourceId = '/subscriptions/<subscriptionID>/resourceGroups/test-analysisServices-parameters.json-rg/providers/Microsoft.Storage/storageAccounts/adpqsxxazsaaspar01'
     type       = 'Microsoft.Storage/storageAccounts'
-    name       = 'adpsxxazsaaspar01'
+    name       = 'adpqsxxazsaaspar01'
 }
 #>
 function Get-ResourceIdsAsFormattedObjectList {
