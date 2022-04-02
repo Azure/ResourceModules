49c8de26d808ef7187e3aff765bd6aac296ca008
MrMCake <asehr@hotmail.de>
2022-04-02 13:27:05 +0200

    First draft for matrix deploy

---
+++ utilities/pipelines/sharedScripts/Get-ModuleParameterFiles.ps1
@@ -0,0 +1,25 @@
+﻿<#
+.SYNOPSIS
+Get the file paths of all parameter files in the given module
+
+.DESCRIPTION
+Get the file paths of all parameter files in the given module
+
+.PARAMETER ModulePath
+Mandatory. The module path to search in
+
+.EXAMPLE
+Get-ModuleParameterFiles -ModulePath 'C:\ResourceModules\arm\Microsoft.Compute\virtualMachines'
+
+Returns the file paths of all parameter files of the virtual machines module.
+#>
+function Get-ModuleParameterFiles {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string] $ModulePath
+    )
+
+    return (Get-ChildItem -Path (Join-Path $ModulePath '.parameters') -Filter '*.json').FullName
+}
