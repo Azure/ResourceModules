22b6dde0f9aee011e112fcfb386151eb7bebd28d
MrMCake <asehr@hotmail.de>
2022-05-16 19:09:55 +0200

    Update to latest

---
+++ utilities/tools/Get-LinkedModuleList.ps1
@@ -0,0 +1,15 @@
+﻿function Get-LinkedModuleList {
+
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string] $path = (Join-Path (Split-Path (Split-Path $PSScriptRoot -Parent) -Parent) 'arm')
+    )
+
+    $resultSet = @{}
+
+    $templatePaths = (Get-ChildItem -Path $path -Recurse -Filter '.bicep').FullName
+
+    $templatePaths
+}
+Get-LinkedModuleList
