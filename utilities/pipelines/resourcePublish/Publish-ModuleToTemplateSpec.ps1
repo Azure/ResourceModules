47d86a05e8fd1d025f038db4b236b022dbdd3989
SeSeicht <48480298+SeSeicht@users.noreply.github.com>
2022-06-26 16:04:29 +0200

    [Pipeline] Users/seseicht/adjust publish (#1472)
    * enable publishing in different subscription
    
    Co-authored-by: Marius Storhaug <marstor@hotmail.com>

--- utilities/pipelines/resourcePublish/Publish-ModuleToTemplateSpec.ps1
+++ utilities/pipelines/resourcePublish/Publish-ModuleToTemplateSpec.ps1
@@ -48,7 +48,10 @@ function Publish-ModuleToTemplateSpec {
         [string] $TemplateSpecsRgLocation,
 
         [Parameter(Mandatory)]
-        [string] $TemplateSpecsDescription
+        [string] $TemplateSpecsDescription,
+
+        [Parameter(Mandatory = $false)]
+        [string] $subscriptionId
     )
 
     begin {
@@ -66,6 +69,14 @@ function Publish-ModuleToTemplateSpec {
             if ($PSCmdlet.ShouldProcess("Resource group [$TemplateSpecsRgName] to location [$TemplateSpecsRgLocation]", 'Deploy')) {
                 New-AzResourceGroup -Name $TemplateSpecsRgName -Location $TemplateSpecsRgLocation
             }
+        }
+
+        #############################
+        ##      set AzContext      ##
+        #############################
+        if (-not [String]::IsNullOrEmpty($subscriptionId)) {
+            Write-Verbose ('Setting context to subscription [{0}]' -f $subscriptionId)
+            $null = Set-AzContext -Subscription $subscriptionId
         }
 
         ################################
