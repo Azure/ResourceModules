733140cefbf11bd6272e5ef1bd0e48b66e9cd565
Alexander Sehr <ASehr@hotmail.de>
2022-07-12 22:48:33 +0200

    Update utilities/tools/Get-ModulesFeatureOutline.ps1
    Co-authored-by: Erika Gressi <56914614+eriqua@users.noreply.github.com>

--- utilities/tools/Get-ModulesFeatureOutline.ps1
+++ utilities/tools/Get-ModulesFeatureOutline.ps1
@@ -182,7 +182,7 @@ function Get-ModulesFeatureOutline {
                     'Tags'       = $_.'Tags'
                     'Diag'       = $_.'Diag'
                     'PE'         = $_.'PE'
-                    'Pip-Depl.'  = $_.'Pip-Depl.'
+                    'PIP'  = $_.'PIP'
                     '# children' = $_.'# children'
                     '# lines'    = $_.'# lines'
                 }
