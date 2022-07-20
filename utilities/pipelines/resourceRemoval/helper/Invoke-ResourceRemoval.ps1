263c83993fd3426d0100d8d6aabc3be8a39072df
Erika Gressi <erikag@microsoft.com>
2022-07-20 10:03:48 +0200

    lowercase

--- utilities/pipelines/resourceRemoval/helper/Invoke-ResourceRemoval.ps1
+++ utilities/pipelines/resourceRemoval/helper/Invoke-ResourceRemoval.ps1
@@ -57,7 +57,7 @@ function Invoke-ResourceRemoval {
             Write-Verbose ('Skip resource removal for type [{0}]. Reason: handled by different logic.' -f $type) -Verbose
             break
         }
-        'Microsoft.ServiceBus/namespaces/AuthorizationRules' {
+        'Microsoft.ServiceBus/namespaces/authorizationRules' {
             if ((Split-Path $ResourceId '/')[-1] -eq 'RootManageSharedAccessKey') {
                 Write-Verbose ('Skip resource removal for type [{0}]. Reason: The Service Bus''s default authorization key [RootManageSharedAccessKey] cannot be removed.' -f $type) -Verbose
             } else {
