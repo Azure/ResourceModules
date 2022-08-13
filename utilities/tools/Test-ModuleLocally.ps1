891e7e9c7ed83089aa07c3cdd2e51a5b668de2b9
Ahmad Abdalla <28486158+ahmadabdalla@users.noreply.github.com>
2022-08-13 15:00:35 +1000

    Update utilities/tools/Test-ModuleLocally.ps1
    Co-authored-by: Alexander Sehr <ASehr@hotmail.de>

--- utilities/tools/Test-ModuleLocally.ps1
+++ utilities/tools/Test-ModuleLocally.ps1
@@ -34,7 +34,7 @@ Optional. A hashtable parameter that contains custom tokens to be replaced in th
 
 $TestModuleLocallyInput = @{
     TemplateFilePath           = 'C:\Microsoft.Network\routeTables\deploy.bicep'
-    ParameterFilePath               = 'C:\Microsoft.Network\routeTables\.test\parameters.json'
+    ParameterFilePath          = 'C:\Microsoft.Network\routeTables\.test\parameters.json'
     PesterTest                 = $false
     DeploymentTest             = $false
     ValidationTest             = $true
