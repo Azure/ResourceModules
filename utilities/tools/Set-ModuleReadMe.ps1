e738423dcd7d81cf3e5f8b6314b8ae8c3d5cde2b
MrMCake <asehr@hotmail.de>
2022-08-14 16:08:54 +0200

    Improved case handling

--- utilities/tools/Set-ModuleReadMe.ps1
+++ utilities/tools/Set-ModuleReadMe.ps1
@@ -655,21 +655,31 @@ function ConvertTo-FormattedJSONParameterObject {
 
     # [2.2] Syntax: All single-quotes are double-quotes
     $paramInJsonFormat = $paramInJsonFormat -replace "'", '"'
-    # [2.3] Syntax: Everything left of a ':' should be wrapped in quotes (as a parameter name is always a string)
-    $paramInJsonFormat = $paramInJsonFormat -replace '([0-9a-zA-Z]+):', '"$1":'
 
-    # [2.4] Split the object to format line-by-line (& also remove any empty lines)
-    $paramInJSONFormatArray = $paramInJsonFormat -split '\n' | Where-Object { $_ }
+    # [2.3] Split the object to format line-by-line (& also remove any empty lines)
+    $paramInJSONFormatArray = $paramInJsonFormat -split '\n' | Where-Object { -not [String]::IsNullOrEmpty($_.Trim()) }
 
-    # [2.5] Syntax: Replace Bicep resource ID references
     for ($index = 0; $index -lt $paramInJSONFormatArray.Count; $index++) {
-        if ($paramInJSONFormatArray[$index] -like '*:*' -and ($paramInJSONFormatArray[$index] -split ':')[1].Trim() -notmatch '".+"' -and $paramInJSONFormatArray[$index] -like '*.*') {
-            # In case of a reference like : "virtualWanId": resourceGroupResources.outputs.virtualWWANResourceId
-            $paramInJSONFormatArray[$index] = '{0}: "<{1}>"' -f ($paramInJSONFormatArray[$index] -split ':')[0], ([regex]::Match(($paramInJSONFormatArray[$index] -split ':')[0], '"(.+)"')).Captures.Groups[1].Value
-        }
-        if ($paramInJSONFormatArray[$index] -notlike '*:*' -and $paramInJSONFormatArray[$index] -notlike '*"*"*' -and $paramInJSONFormatArray[$index] -like '*.*') {
-            # In case of a reference like : [ \n resourceGroupResources.outputs.managedIdentityPrincipalId \n ]
-            $paramInJSONFormatArray[$index] = '"<{0}>"' -f $paramInJSONFormatArray[$index].Split('.')[-1].Trim()
+
+        $lineValue = $paramInJSONFormatArray[$index]
+
+        # [2.4] Syntax: Everything left of a leftest ':' should be wrapped in quotes (as a parameter name is always a string)
+        [regex]$pattern = '\:{0}([0-9a-zA-Z]+):'
+        $lineValue = $pattern.replace($lineValue, '"$1":', 1)
+
+        # [2.5] Syntax: Replace Bicep resource ID references
+        if ($lineValue -like '*:*' -and $lineValue -notmatch '^.+:\s*{\s*}\s*$' -and ($lineValue -split ':')[1].Trim() -notmatch '".+"' -and $lineValue -like '*.*') {
+            # In case of an output reference like '"virtualWanId": resourceGroupResources.outputs.virtualWWANResourceId' we'll only show "<virtualWanId>" (but NOT e.g. 'reference': {})
+            $paramInJSONFormatArray[$index] = '{0}: "<{1}>"' -f ($lineValue -split ':')[0], ([regex]::Match(($lineValue -split ':')[0], '"(.+)"')).Captures.Groups[1].Value
+        } elseif ($lineValue -like '*:*' -and ($lineValue -split ':')[1].Trim() -match '[a-zA-Z]+\(.+\)') {
+            # In case of a any contained function like '"backupVaultResourceGroup": (split(resourceGroupResources.outputs.recoveryServicesVaultResourceId, "/"))[4]' we'll only show "<backupVaultResourceGroup>"
+            $paramInJSONFormatArray[$index] = '{0}: "<{1}>"' -f ($lineValue -split ':')[0], ([regex]::Match(($lineValue -split ':')[0], '"(.+)"')).Captures.Groups[1].Value
+        } elseif ($lineValue -like '*:*' -and ($lineValue -split ':')[1].Trim() -match '^\w+$' -and ($lineValue -split ':')[1].Trim() -notmatch '^\s*true|false|[0-9]+$') {
+            # In case of a parameter/variable reference like 'adminPassword: password' we'll only show "<adminPassword>" (but NOT e.g. enableMe: true)
+            $paramInJSONFormatArray[$index] = '{0}: "<{1}>"' -f ($lineValue -split ':')[0], ([regex]::Match(($lineValue -split ':')[0], '"(.+)"')).Captures.Groups[1].Value
+        } elseif ($lineValue -notlike '*:*' -and $lineValue -notlike '*"*"*' -and $lineValue -like '*.*') {
+            # In case of a array value like '[ \n -> resourceGroupResources.outputs.managedIdentityPrincipalId <- \n ]' we'll only show "<managedIdentityPrincipalId>""
+            $paramInJSONFormatArray[$index] = '"<{0}>"' -f $lineValue.Split('.')[-1].Trim()
         }
     }
 
