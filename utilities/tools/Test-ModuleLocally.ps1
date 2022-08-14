dccd73a2595f64fce4892344b2635ca183c43a3e
MrMCake <asehr@hotmail.de>
2022-08-14 23:14:25 +0200

    Update to latest

--- utilities/tools/Test-ModuleLocally.ps1
+++ utilities/tools/Test-ModuleLocally.ps1
@@ -247,14 +247,11 @@ function Test-ModuleLocally {
                 # -----------------
                 if ($ValidationTest) {
                     if ($moduleTestFiles) {
-                        # Loop through test parameter files
+                        # Loop through test files
                         foreach ($moduleTestFile in $moduleTestFiles) {
                             Write-Verbose ('Validating module [{0}] with test file [{1}]' -f $ModuleName, (Split-Path $moduleTestFile -Leaf)) -Verbose
                             Test-TemplateDeployment @functionInput -ParameterFilePath $moduleTestFile
                         }
-                    } else {
-                        Write-Verbose ('Validating module [{0}]' -f $ModuleName) -Verbose
-                        Test-TemplateDeployment @functionInput
                     }
                 }
 
@@ -262,18 +259,11 @@ function Test-ModuleLocally {
                 # ---------------
                 if ($DeploymentTest) {
                     $functionInput['retryLimit'] = 1 # Overwrite default of 3
-                    # Loop through test parameter files
-                    if ($moduleTestFile) {
-                        foreach ($moduleTestFile in $moduleTestFiles) {
-                            Write-Verbose ('Deploy Module [{0}] with test file [{1}]' -f $ModuleName, (Split-Path $moduleTestFile -Leaf)) -Verbose
-                            if ($PSCmdlet.ShouldProcess(('Module [{0}] with test file [{1}]' -f $ModuleName, (Split-Path $moduleTestFile -Leaf)), 'Deploy')) {
-                                New-TemplateDeployment @functionInput -ParameterFilePath $moduleTestFile
-                            }
-                        }
-                    } else {
-                        Write-Verbose ('Deploy Module [{0}]' -f $ModuleName) -Verbose
-                        if ($PSCmdlet.ShouldProcess(('Module [{0}]' -f $ModuleName), 'Deploy')) {
-                            New-TemplateDeployment @functionInput
+                    # Loop through test files
+                    foreach ($moduleTestFile in $moduleTestFiles) {
+                        Write-Verbose ('Deploy Module [{0}] with test file [{1}]' -f $ModuleName, (Split-Path $moduleTestFile -Leaf)) -Verbose
+                        if ($PSCmdlet.ShouldProcess(('Module [{0}] with test file [{1}]' -f $ModuleName, (Split-Path $moduleTestFile -Leaf)), 'Deploy')) {
+                            New-TemplateDeployment @functionInput -ParameterFilePath $moduleTestFile
                         }
                     }
                 }
@@ -281,8 +271,8 @@ function Test-ModuleLocally {
             } catch {
                 Write-Error $_
             } finally {
-                # Restore parameter files
-                # -----------------------
+                # Restore test files
+                # ------------------
                 if (($ValidationTest -or $DeploymentTest) -and $ValidateOrDeployParameters -and $moduleTestFiles) {
                     # Replace Values with Tokens For Repo Updates
                     Write-Verbose 'Restoring Tokens'
@@ -294,3 +284,4 @@ function Test-ModuleLocally {
     end {
     }
 }
+
