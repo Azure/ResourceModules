568325a6a10c4ffd5c5c1dc8a4e6e7f8ae69aede
MrMCake <asehr@hotmail.de>
2022-09-03 10:04:23 +0200

    sync

--- modules/Microsoft.Network/applicationGateways/.test/.scripts/New-Certificate.ps1
+++ modules/Microsoft.Network/applicationGateways/.test/.scripts/New-Certificate.ps1
@@ -3,29 +3,31 @@
     [string] $CertName
 )
 
-$policyInputObject = @{
-    SecretContentType = 'application/x-pkcs12'
-    SubjectName       = 'CN=fabrikam.com'
-    IssuerName        = 'Self'
-    ValidityInMonths  = 12
-    ReuseKeyOnRenewal = $true
+if (-not ($certificate = Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertName -ErrorAction 'Stop')) {
+    $policyInputObject = @{
+        SecretContentType = 'application/x-pkcs12'
+        SubjectName       = 'CN=fabrikam.com'
+        IssuerName        = 'Self'
+        ValidityInMonths  = 12
+        ReuseKeyOnRenewal = $true
+    }
+    $certPolicy = New-AzKeyVaultCertificatePolicy @policyInputObject
+
+    $null = Add-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertName -CertificatePolicy $certPolicy
+    Write-Verbose ('Initated creation of certificate [{0}] in key vault [{1}]' -f $CertName, $KeyVaultName) -Verbose
+
+    while (-not (Get-AzKeyVaultCertificateOperation -VaultName $KeyVaultName -Name $CertName).Status -eq 'completed') {
+        Write-Verbose 'Waiting 10 seconds for certificate creation' -Verbose
+        Start-Sleep 10
+    }
+
+    Write-Verbose 'Certificate created' -Verbose
+
+
+    $certificate = Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertName -ErrorAction 'Stop'
 }
-$certPolicy = New-AzKeyVaultCertificatePolicy @policyInputObject
-
-$null = Add-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertName -CertificatePolicy $certPolicy
-Write-Verbose ('Initated creation of certificate [{0}] in key vault [{1}]' -f $CertName, $KeyVaultName) -Verbose
-
-while (-not (Get-AzKeyVaultCertificateOperation -VaultName $KeyVaultName -Name $CertName).Status -eq 'completed') {
-    Write-Verbose 'Waiting 10 seconds for certificate creation' -Verbose
-    Start-Sleep 10
-}
-
-Write-Verbose 'Certificate created' -Verbose
-
-$certificate = Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertName
 
 # Write into Deployment Script output stream
 $DeploymentScriptOutputs = @{
-    # Requires conversion as the script otherwise returns an object instead of the plain public key string
     secretUrl = $certificate.SecretId
 }
