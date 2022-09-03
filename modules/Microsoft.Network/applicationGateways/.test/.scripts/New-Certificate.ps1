a1b196875c1d07a0ac225b66a4ff8c8202fec987
MrMCake <asehr@hotmail.de>
2022-09-03 14:13:11 +0200

    Updaed script

--- modules/Microsoft.Network/applicationGateways/.test/.scripts/New-Certificate.ps1
+++ modules/Microsoft.Network/applicationGateways/.test/.scripts/New-Certificate.ps1
@@ -3,7 +3,9 @@
     [string] $CertName
 )
 
-if (-not ($certificate = Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertName -ErrorAction 'Stop')) {
+$certificate = Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertName -ErrorAction 'Stop'
+
+if (-not $certificate) {
     $policyInputObject = @{
         SecretContentType = 'application/x-pkcs12'
         SubjectName       = 'CN=fabrikam.com'
@@ -22,14 +24,17 @@ if (-not ($certificate = Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Nam
     }
 
     Write-Verbose 'Certificate created' -Verbose
+}
 
-    while (-not ($certificate = Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertName -ErrorAction 'Stop')) {
-        Write-Verbose 'Waiting 10 seconds until certificate can be fetched' -Verbose
-        Start-Sleep 10
-    }
+$secretId = $certificate.SecretId
+while ([String]::IsNullOrEmpty($secretId)) {
+    Write-Verbose 'Waiting 10 seconds until certificate can be fetched' -Verbose
+    Start-Sleep 10
+    $certificate = Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertName -ErrorAction 'Stop'
+    $secretId = $certificate.SecretId
 }
 
 # Write into Deployment Script output stream
 $DeploymentScriptOutputs = @{
-    secretUrl = $certificate.SecretId
+    secretUrl = $secretId
 }
