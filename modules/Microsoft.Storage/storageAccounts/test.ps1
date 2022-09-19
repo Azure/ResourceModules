b8cb811c843ed86db407e4209d7b2790a77e5d2b
Sebastian Graef <segraef@microsoft.com>
2022-09-19 17:24:34 +1000

    works

--- modules/Microsoft.Storage/storageAccounts/test.ps1
+++ modules/Microsoft.Storage/storageAccounts/test.ps1
@@ -1,15 +1,4 @@
+az acr update --name adpsxxazacrx009 --anonymous-pull-enabled
 
-resourceGroupName = "validate-rg"
-templateFile="main.bicep"
-
-az group create --name $resourceGroupName --location australiaeast
-
-az deployment group create --resource-group $resourceGroupName --template-file $templateFile
-
-
-alias specifying the registry
-`module stgModule 'br/pbr:bicep/modules/core/storage:v1' = {`
-
-alias that specifies the registry and module path.
-
-`module stgModule  'br/pbr:storage:v1' = {`
+az group create --name validate-rg --location australiaeast
+az deployment group create --resource-group validate-rg --template-file main.bicep
