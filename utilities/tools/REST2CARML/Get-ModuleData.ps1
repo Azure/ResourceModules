95c711262425dfb6bdaf0983441d485a4c04cdd4
MrMCake <asehr@hotmail.de>
2022-09-19 10:35:13 +0200

    Added dummy REST Fetch script

---
+++ utilities/tools/REST2CARML/Get-ModuleData.ps1
@@ -0,0 +1,16 @@
+﻿# Alt: https://docs.microsoft.com/en-us/azure/templates
+$response = Invoke-WebRequest -Uri 'https://docs.microsoft.com/en-us/rest/api/azure/toc.json'
+
+if (-not $response.Content) {
+    throw 'Fetch failed'
+}
+
+$menu = ($response.Content | ConvertFrom-Json -Depth 99 -AsHashtable).items
+
+$topLevelResourceTypes = $menu.toc_title | Where-Object { $_ -notlike 'Getting Started with REST' }
+
+foreach ($resourceType in $topLevelResourceTypes) {
+    # Fetch children
+    # Filter down by 'CREATE' commands
+    # Run recursively
+}
