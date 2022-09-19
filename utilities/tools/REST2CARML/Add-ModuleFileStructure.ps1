edd5b220e5a89715709f9f5fbe3858c0784fe98f
prteotia <97890681+prteotia@users.noreply.github.com>
2022-09-19 18:48:49 +0530

    created new file for adding module structure
    Generates module structure

---
+++ utilities/tools/REST2CARML/Add-ModuleFileStructure.ps1
@@ -0,0 +1,49 @@
+﻿
+
+function Add-ModuleFileStructure{
+    param(
+         [Parameter(Mandatory)]
+         [string] $ProviderNamespace,
+
+         [Parameter(Mandatory)]
+         [string] $ResourceType,
+
+
+
+         $filesArray=@('deploy.bicep', 'readme.md', 'version.json'),
+
+         $gitHubWorkflowYAMLPath = "github/workflows/",
+         $azDevOpsModulePipelineYAMLPath = ".azureDevOps/modulePipelines/"
+
+    )
+
+    try{
+        $repoRootPath = (Get-Item $PSScriptRoot).Parent.Parent
+        $ModulePath = Join-path -path $repoRootPath 'modules'
+
+        $ModuleName = "$ProviderNamespace\$ResourceType"
+        $ProviderDir = New-Item -Path $ModulePath -Name $ModuleName -ItemType "directory"
+
+       write-Host $ProviderDir
+
+
+        if($filesArray){
+            foreach($fileName in $filesArray){
+                if($fileName){
+                    write-Host "Creating File: $fileName"
+                    New-Item -Path $ProviderDir -Name $fileName -ItemType "file"
+                }
+            }
+        }
+
+        $workflowYAMLPath = Join-path -path $repoRootPath $gitHubWorkflowYAMLPath
+        write-Host $workflowYAMLPath
+
+    }catch{
+        Write-Host "An error occurred:"
+        Write-Host $_
+    }
+
+}
+
+ Add-ModuleFileStructure
