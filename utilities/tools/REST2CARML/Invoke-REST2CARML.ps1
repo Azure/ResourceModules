4dc71b390895af5a1a54a573bf1aa5920cbc4a0c
MrMCake <asehr@hotmail.de>
2022-09-19 10:43:25 +0200

    Update to latest

---
+++ utilities/tools/REST2CARML/Invoke-REST2CARML.ps1
@@ -0,0 +1,51 @@
+﻿<#
+.SYNOPSIS
+Create/Update a CARML module based on the latest API information available
+
+.DESCRIPTION
+Create/Update a CARML module based on the latest API information available
+
+.PARAMETER ProviderNamespace
+Mandatory. The provider namespace to query the data for
+
+.PARAMETER ResourceType
+Mandatory. The resource type to query the data for
+
+.EXAMPLE
+Invoke-REST2CARML -ProviderNamespace 'Microsoft.Keyvault' -ResourceType 'vaults'
+
+Generate/Update a CARML module for [Microsoft.Keyvault/vaults]
+#>
+function Invoke-REST2CARML {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $ProviderNamespace,
+
+        [Parameter(Mandatory = $true)]
+        [string] $ResourceType
+    )
+
+    begin {
+        Write-Debug ('{0} entered' -f $MyInvocation.MyCommand)
+
+        Write-Verbose ('Processing module [{0}/{1}]' -f $ProviderNamespace, $ResourceType) -Verbose
+    }
+
+    process {
+        # TODO: Invoke function to fetch module data
+
+        if ($PSCmdlet.ShouldProcess(('Module [{0}/{1}] structure' -f $ProviderNamespace, $ResourceType), 'Create/Update')) {
+            # TODO: Invoke function to create intial module structure & create workflow/pipeline files
+        }
+
+        if ($PSCmdlet.ShouldProcess(('Module [{0}/{1}] files' -f $ProviderNamespace, $ResourceType), 'Create/Update')) {
+            # TODO: Invoke function to fill module files with module data
+        }
+    }
+
+    end {
+        Write-Debug ('{0} exited' -f $MyInvocation.MyCommand)
+    }
+}
