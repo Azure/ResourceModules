b2788ec341c700f3d485ced67bc8e880a2a02f6d
Kris Baranek <k.baranek@microsoft.com>
2022-09-20 14:49:59 +0200

    Get-ModuleDataSource function

--- utilities/tools/REST2CARML/Get-ModuleDataRestApiSpecs.ps1
+++ utilities/tools/REST2CARML/Get-ModuleDataRestApiSpecs.ps1
@@ -111,4 +111,126 @@ function Get-ModuleDataRestApiSpecs {
 }
 
 
+function Get-ModuleDataSource {
+
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $ProviderNamespace,
+
+        [Parameter(Mandatory = $true)]
+        [string] $ResourceType,
+
+        [Parameter(Mandatory = $false)]
+        [bool] $IgnorePreview = $true
+    )
+
+    # prepare the repo
+    try {
+        $initialLocation = (Get-Location).Path
+        $repoUrl = 'https://github.com/Azure/azure-rest-api-specs.git'
+        $repoName = Split-Path $repoUrl -LeafBase
+        $tempFolderName = 'temp'
+        $tempFolderPath = Join-Path $PSScriptRoot $tempFolderName
+
+        # Clone repository
+        ## Create temp folder
+        if (-not (Test-Path $tempFolderPath)) {
+            $null = New-Item -Path $tempFolderPath -ItemType 'Directory'
+        }
+        ## Switch to temp folder
+        Set-Location $tempFolderPath
+
+        ## Clone repository into temp folder
+        if (-not (Test-Path (Join-Path $tempFolderPath $repoName))) {
+            git clone $repoUrl
+        } else {
+            Write-Verbose "Repository [$repoName] already cloned"
+        }
+    } catch {
+        throw "Repo preparation failed: $_"
+    }
+
+    #find the resource provider folder
+    # Process repository
+    $shortenedProviderNamespace = ($ProviderNamespace -split '\.')[1].ToLower()
+    $resourceProviderFolder = Join-Path $tempFolderPath $repoName 'specification' $shortenedProviderNamespace 'resource-manager' $ProviderNamespace
+    if (-not (Test-Path $resourceProviderFolder)) {
+        $resourceProviderFolderSearchResults = Get-ChildItem -Path $specifications -Directory -Recurse -Depth 3 | Where-Object { $_.Name -eq $ProviderNamespace -and $_.Parent.Name -eq 'resource-manager' }
+        switch ($resourceProviderFolderSearchResults.Count) {
+            { $_ -eq 0 } { throw ('Resource provider folder [{0}] not found' -f $ProviderNamespace); break }
+            { $_ -ge 1 } { $resourceProviderFolder = $resourceProviderFolderSearchResults[0].FullName }
+            { $_ -gt 1 } {
+                Write-Warning ('Other folder(s) with the name [{0}] found.' -f $ProviderNamespace)
+                for ($i = 1; $i -lt $resourceProviderFolderSearchResults.Count; $i++) {
+                    Write-Warning ('  {0}' -f $resourceProviderFolderSearchResults[$i].FullName)
+                }
+                break
+            }
+            Default {}
+        }
+    }
+    Write-Verbose ('Processing Resource provider folder [{0}]' -f $resourceProviderFolder) -Verbose
+
+    try {
+        # TODO: Get highest API version (preview/non-preview)
+        $apiVersionFoldersArr = @()
+        if (Test-Path -Path $(Join-Path $resourceProviderFolder 'stable')) { $apiVersionFoldersArr += Get-ChildItem -Path $(Join-Path $resourceProviderFolder 'stable') }
+        if (-not $IgnorePreview) {
+            # adding preview API versions if allowed
+            if (Test-Path -Path $(Join-Path $resourceProviderFolder 'preview')) { $apiVersionFoldersArr += Get-ChildItem -Path $(Join-Path $resourceProviderFolder 'preview') }
+        }
+
+        # sorting all API version from the newest to the oldest
+        $apiVersionFoldersArr = $apiVersionFoldersArr | Sort-Object -Property Name -Descending
+        if ($apiVersionFoldersArr.Count -eq 0) {
+            throw ('No API folder found in folder [{0}]' -f $resourceProviderFolder)
+        }
+
+        foreach ($apiversionFolder in $apiVersionFoldersArr) {
+            $putMethods = @()
+            foreach ($jsonFile in $(Get-ChildItem -Path $apiversionFolder -Filter *.json)) {
+                $jsonPaths = (ConvertFrom-Json (Get-Content -Raw -Path $jsonFile)).paths
+                $jsonPaths.PSObject.Properties | ForEach-Object {
+                    $put = $_.value.put
+                    if ($put) {
+                        $pathSplit = $_.Name.Split('/')
+                        if (($pathSplit[$pathSplit.Count - 3] -eq $ProviderNamespace) -and ($pathSplit[$pathSplit.Count - 2] -eq $ResourceType)) {
+                            $arrItem = [pscustomobject] @{}
+                            $arrItem | Add-Member -MemberType NoteProperty -Name 'jsonFilePath' -Value $jsonFile.FullName
+                            $arrItem | Add-Member -MemberType NoteProperty -Name 'jsonKeyPath' -Value $_.Name
+                            # $arrItem | Add-Member -MemberType NoteProperty -Name 'putMethod' -Value $_.value.put
+                            $putMethods += $arrItem
+                        }
+                    }
+                }
+            }
+            if ($putMethods.Count -gt 0) { break }
+        }
+
+
+        Set-Location $initialLocation
+
+        ## Remove temp folder again
+        # $null = Remove-Item $tempFolderPath -Recurse -Force
+
+        if ($putMethods.Count -eq 1) {
+            # return 1
+            return $putMethods[0]
+        } elseif ($putMethods.Count -gt 1) {
+            # return $putMethods.Count
+            Write-Error 'Found too many matching results'
+        } else {
+            # return 0
+            Write-Error 'No results found'
+        }
+
+    } catch {
+        # return -2
+        Write-Error "Error processing [$ProviderNamespace/$ResourceType]: $_"
+    }
+}
+
 Get-ModuleDataRestApiSpecs -ProviderNamespace 'Microsoft.KeyVault' -ResourceType 'vaults'
+
+Get-ModuleDataSource -ProviderNamespace 'Microsoft.KeyVault' -ResourceType 'vaults' | Format-List
+
