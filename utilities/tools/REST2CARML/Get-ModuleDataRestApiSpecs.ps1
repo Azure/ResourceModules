962dc8eb9c5566c8eb0ae9263b059045c0f9621e
George Dobrin <georgedobrin@microsoft.com>
2022-09-20 13:29:05 +0200

    Update Get-ModuleDataRestApiSpecs.ps1

--- utilities/tools/REST2CARML/Get-ModuleDataRestApiSpecs.ps1
+++ utilities/tools/REST2CARML/Get-ModuleDataRestApiSpecs.ps1
@@ -1,4 +1,43 @@
-﻿function Get-ModuleDataRestApiSpecs {
+﻿function FilterPutPaths {
+    Param($params)
+    $putObj = $params[0]
+    $definitions = $params[1]
+    $obj = {}
+    $putObj.parameters | ForEach-Object {
+        if ($_.name -eq 'parameters') {
+            $newObj = Get-NestedParams($_.schema.$ref, $definitions)
+            # $obj | Add-Member -MemberType NoteProperty -Name $newObj.name -Value $newObj
+        } elseif ($null -ne $_.name) {
+            $paramItem = [PSCustomObject]@{
+                name        = $_.name
+                type        = $_.type
+                description = $_.description
+            }
+
+            $obj | Add-Member -MemberType NoteProperty -Name $paramItem.name -Value $paramItem
+        } elseif ($null -ne $_.$ref) {
+            $newObj = Get-NestedParams($_.$ref, $definitions)
+            # $obj | Add-Member -MemberType NoteProperty -Name $newObj.name -Value $newObj
+        }
+
+    }
+    return $obj
+}
+
+function Get-NestedParams {
+    # check why ref is null here
+    Param($params)
+    $ref = $params[0]
+    $definitions = $params[1]
+    $refDef = Split-Path $ref -LeafBase
+    if ($refDef -notin $definitions) {
+        throw 'ref is not contained in definition'
+    } else {
+        # strip $definitions.$refDef and return
+    }
+}
+
+function Get-ModuleDataRestApiSpecs {
 
     param (
         [Parameter(Mandatory = $true)]
@@ -42,14 +81,19 @@
     $putMethods = @()
     foreach ($jsonFile in $(Get-ChildItem -Path $latestApiFolder -Filter *.json)) {
         $jsonPaths = (ConvertFrom-Json (Get-Content -Raw -Path $jsonFile)).paths
+
         $jsonPaths.PSObject.Properties | ForEach-Object {
             $put = $_.value.put
+            $definitions = (ConvertFrom-Json (Get-Content -Raw -Path $jsonFile)).definitions
 
             if ($put) {
                 $arrItem = [pscustomobject] @{}
                 $arrItem | Add-Member -MemberType NoteProperty -Name 'jsonFile' -Value $jsonFile.Name
                 $arrItem | Add-Member -MemberType NoteProperty -Name 'path' -Value $_.Name
                 # $arrItem | Add-Member -MemberType NoteProperty -Name 'putMethod' -Value $_.value.put
+
+                $filteredObj = FilterPutPaths($put, $definitions)
+                $arrItem | Add-Member -MemberType NoteProperty -Name 'properties' -Value $filteredObj
 
                 $putMethods += $arrItem
             }
@@ -58,10 +102,13 @@
 
     $putMethods | ConvertTo-Json
 
+    ## process file
+
 
     ## Remove temp folder again
     # $null = Remove-Item $tempFolderPath -Recurse -Force
     Set-Location $initialLocation
 }
+
 
 Get-ModuleDataRestApiSpecs -ProviderNamespace 'Microsoft.KeyVault' -ResourceType 'vaults'
