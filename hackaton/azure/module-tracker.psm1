dc52263627b70ac1c46e7cfd658e8161b9996095
Tomaz Mlakar <mlakar.tomaz@outlook.com>
2022-09-21 11:50:18 +0200

    Adding additional functions to the helper module

--- hackaton/azure/module-tracker.psm1
+++ hackaton/azure/module-tracker.psm1
@@ -1,5 +1,4 @@
-﻿
-<#
+﻿<#
 .SYNOPSIS
 Get hash of the ARM deployment JSON template using only 'resources' tag
 
@@ -55,4 +54,104 @@ function Get-TemplateHash {
     }
 }
 
+<#
+.SYNOPSIS
+Create a new table in the Storage Account provided
+
+.DESCRIPTION
+Create a new table in the Storage Account provided
+
+.PARAMETER StorageAccountName
+Required. Storage account name
+
+.PARAMETER ResourceGroup
+Required. Resource group name
+
+.PARAMETER TableName
+Required. Table name
+
+
+.EXAMPLE
+New-StorageAccountTable -StorageAccountName 'stgstore01' -ResourceGroup 'data-rg' -TableName 'data'
+
+#>
+
+function New-StorageAccountTable {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [string] $StorageAccountName,
+
+        [Parameter(Mandatory = $false)]
+        [string] $ResourceGroup,
+
+        [Parameter(Mandatory = $false)]
+        [string] $TableName
+    )
+
+    try {
+        # get storage Account and context
+        $storageAccount = Get-AzStorageAccount -Name $StorageAccountName -ResourceGroupName $ResourceGroup
+        $ctx = $storageAccount.Context
+
+        # create the table
+        New-AzStorageTable -Name $TableName -Context $ctx -ErrorAction SilentlyContinue | Out-Null
+        $table = (Get-AzStorageTable -Name $TableName -Context $ctx).CloudTable
+
+        return $table
+    } catch {
+        throw $_
+    }
+}
+
+<#
+.SYNOPSIS
+Adds a new row to the Storage Account table
+
+.DESCRIPTION
+Adds a new row to the Storage Account table
+
+.PARAMETER Table
+Required. Stoage Account Table object
+
+.PARAMETER PartitionKey
+Required. PartitionKey of the row to be added, usualy deployment Id
+
+.PARAMETER DeploymentName
+Required. Name of the deployment
+
+.PARAMETER Hash
+Required. Hash value
+
+.EXAMPLE
+New-StorageAccountTableRow -Table $table -PartitionKey '/subscriptions/00000000-0000-0000-0000-000000000000/providers/Microsoft.Resources/deployments/main-bcb4-0fd963ffbd82-z4h2pdgyedqg6/operations/08585380170111792859' -DeploymentName 'main-bcb4-0fd963ffbd82-z4h2pdgyedqg6' -Hash 'B448693A6E15F3E3512E6C6A4C72E34856B28412227184280997579DD3A767CE'
+
+#>
+
+function New-StorageAccountTableRow {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [string] $Table,
+
+        [Parameter(Mandatory = $false)]
+        [string] $PartitionKey,
+
+        [Parameter(Mandatory = $false)]
+        [string] $DeploymentName,
+
+        [Parameter(Mandatory = $false)]
+        [string] $Hash
+    )
+
+    try {
+        $extractionDate = Get-Date -Format 'yyyy-mm-dd hh:mm:ss'
+        Add-AzTableRow -table $Table -partitionKey $PartitionKey -rowKey $DeploymentName -property @{'Hash' = $Hash; 'Time' = $extractionDate } -UpdateExisting | Out-Null
+    } catch {
+        throw $_
+    }
+}
+
 Export-ModuleMember -Function 'Get-TemplateHash'
+Export-ModuleMember -Function 'New-StorageAccountTable'
+Export-ModuleMember -Function 'New-StorageAccountTableRow'
