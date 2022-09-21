12a11d13cbb2352ed11c5778102e20c10c924043
George Dobrin <georgedobrin@microsoft.com>
2022-09-21 10:22:42 +0200

    Update Get-ModuleDataRestApiSpecs.ps1

--- utilities/tools/REST2CARML/Get-ModuleDataRestApiSpecs.ps1
+++ utilities/tools/REST2CARML/Get-ModuleDataRestApiSpecs.ps1
@@ -1,7 +1,5 @@
-﻿function FilterPutPaths {
-    Param($params)
-    $putObj = $params[0]
-    $definitions = $params[1]
+﻿function FilterParameters ($putObj, $definitions) {
+    # TODO: recheck params
     $obj = {}
     $putObj.parameters | ForEach-Object {
         if ($_.name -eq 'parameters') {
@@ -19,13 +17,12 @@
             $newObj = Get-NestedParams($_.$ref, $definitions)
             # $obj | Add-Member -MemberType NoteProperty -Name $newObj.name -Value $newObj
         }
-
     }
     return $obj
 }
 
 function Get-NestedParams {
-    # check why ref is null here
+    # TODO: check why ref is null here
     Param($params)
     $ref = $params[0]
     $definitions = $params[1]
@@ -33,81 +30,8 @@ function Get-NestedParams {
     if ($refDef -notin $definitions) {
         throw 'ref is not contained in definition'
     } else {
-        # strip $definitions.$refDef and return
+        #TODO: strip $definitions.$refDef and return
     }
-}
-
-function Get-ModuleDataRestApiSpecs {
-
-    param (
-        [Parameter(Mandatory = $true)]
-        [string] $ProviderNamespace,
-
-        [Parameter(Mandatory = $true)]
-        [string] $ResourceType,
-
-        [Parameter(Mandatory = $false)]
-        [bool] $IgnorePreview = $true
-    )
-
-    $initialLocation = (Get-Location).Path
-    $repoUrl = 'https://github.com/Azure/azure-rest-api-specs.git'
-    $repoName = Split-Path $repoUrl -LeafBase
-    $tempFolderName = 'temp'
-    $tempFolderPath = Join-Path $PSScriptRoot $tempFolderName
-
-    # Clone repository
-    ## Create temp folder
-    if (-not (Test-Path $tempFolderPath)) {
-        $null = New-Item -Path $tempFolderPath -ItemType 'Directory'
-    }
-    ## Switch to temp folder
-    Set-Location $tempFolderPath
-
-    ## Clone repository into temp folder
-    if (-not (Test-Path (Join-Path $tempFolderPath $repoName))) {
-        git clone $repoUrl
-    } else {
-        Write-Verbose "Repository [$repoName] already cloned" -Verbose
-    }
-
-    # Process repository
-    $shortenedProviderNamespace = ($ProviderNamespace -split '\.')[1].ToLower()
-    $resourceProviderFolder = Join-Path $tempFolderPath $repoName 'specification' $shortenedProviderNamespace 'resource-manager' $ProviderNamespace
-
-    # TODO: Get highest API version (preview/non-preview)
-    $latestApiFolder = (Get-ChildItem -Path $(Join-Path $resourceProviderFolder 'stable') | Sort-Object -Descending)[0]
-
-    $putMethods = @()
-    foreach ($jsonFile in $(Get-ChildItem -Path $latestApiFolder -Filter *.json)) {
-        $jsonPaths = (ConvertFrom-Json (Get-Content -Raw -Path $jsonFile)).paths
-
-        $jsonPaths.PSObject.Properties | ForEach-Object {
-            $put = $_.value.put
-            $definitions = (ConvertFrom-Json (Get-Content -Raw -Path $jsonFile)).definitions
-
-            if ($put) {
-                $arrItem = [pscustomobject] @{}
-                $arrItem | Add-Member -MemberType NoteProperty -Name 'jsonFile' -Value $jsonFile.Name
-                $arrItem | Add-Member -MemberType NoteProperty -Name 'path' -Value $_.Name
-                # $arrItem | Add-Member -MemberType NoteProperty -Name 'putMethod' -Value $_.value.put
-
-                $filteredObj = FilterPutPaths($put, $definitions)
-                $arrItem | Add-Member -MemberType NoteProperty -Name 'properties' -Value $filteredObj
-
-                $putMethods += $arrItem
-            }
-        }
-    }
-
-    $putMethods | ConvertTo-Json
-
-    ## process file
-
-
-    ## Remove temp folder again
-    # $null = Remove-Item $tempFolderPath -Recurse -Force
-    Set-Location $initialLocation
 }
 
 
@@ -190,6 +114,8 @@ function Get-ModuleDataSource {
             $putMethods = @()
             foreach ($jsonFile in $(Get-ChildItem -Path $apiversionFolder -Filter *.json)) {
                 $jsonPaths = (ConvertFrom-Json (Get-Content -Raw -Path $jsonFile)).paths
+                $definitions = (ConvertFrom-Json (Get-Content -Raw -Path $jsonFile)).definitions
+
                 $jsonPaths.PSObject.Properties | ForEach-Object {
                     $put = $_.value.put
                     # if ($_.Name -contains $ResourceType) {
@@ -204,8 +130,12 @@ function Get-ModuleDataSource {
                             # $arrItem | Add-Member -MemberType NoteProperty -Name 'putMethod' -Value $_.value.put
                             $putMethods += $arrItem
                         }
+
+                        # add nested parameters
+                        FilterParameters($put, $definitions)
                     }
                 }
+
             }
             if ($putMethods.Count -gt 0) { break }
         }
