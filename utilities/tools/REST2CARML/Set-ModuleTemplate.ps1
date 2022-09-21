2f8b57b8df202241568100104dd6f80333e5bcca
Kris Baranek <k.baranek@microsoft.com>
2022-09-21 15:35:53 +0200

    Set-ModuleTemplate initial empty file

---
+++ utilities/tools/REST2CARML/Set-ModuleTemplate.ps1
@@ -0,0 +1,35 @@
+﻿function Set-ModuleTempalate {
+
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $ProviderNamespace,
+
+        [Parameter(Mandatory = $true)]
+        [string] $ResourceType,
+
+        [Parameter(Mandatory = $true)]
+        [Hashtable] $ModuleData
+    )
+
+    begin {
+        Write-Debug ('{0} entered' -f $MyInvocation.MyCommand)
+
+        $repoRootPath = (Get-Item $PSScriptRoot).Parent.Parent.Parent
+        $templatePath = Join-Path $repoRootPath 'modules' $ProviderNamespace $ResourceType 'deploy.bicep'
+        # Load used functions
+        . (Join-Path $PSScriptRoot 'Get-DiagnosticOptionsList.ps1')
+        . (Join-Path $PSScriptRoot 'Get-SupportsPrivateEndpoint.ps1')
+        . (Join-Path $repoRootPath 'utilities' 'tools' 'Set-ModuleReadMe.ps1')
+    }
+
+    process {
+
+
+    }
+
+    end {
+        Write-Debug ('{0} exited' -f $MyInvocation.MyCommand)
+    }
+
+}
