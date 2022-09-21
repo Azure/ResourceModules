12742e60e55f50988fd509ef53b36127a964f49e
Kris Baranek <k.baranek@microsoft.com>
2022-09-21 17:27:30 +0200

    Set-ModuleTemplate - first draft params section

--- utilities/tools/REST2CARML/Set-ModuleTemplate.ps1
+++ utilities/tools/REST2CARML/Set-ModuleTemplate.ps1
@@ -1,4 +1,84 @@
-﻿function Set-ModuleTempalate {
+﻿function Get-SectionDivider {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $SectionName
+    )
+    $outerMarginLength = 2
+    $spacesOuterLines = 1
+    $additionalSpacesMiddleLine = 2
+
+    $outerDividerLine = $('/' * $outerMarginLength) + $(' ' * $spacesOuterLines) + $('=' * $additionalSpacesMiddleLine) + $('=' * $SectionName.Length) + $('=' * $additionalSpacesMiddleLine) + $(' ' * $spacesOuterLines) + $('/' * $outerMarginLength)
+    $middleDividerLine = $('/' * $outerMarginLength) + $(' ' * $spacesOuterLines) + $(' ' * $additionalSpacesMiddleLine) + $SectionName + $(' ' * $additionalSpacesMiddleLine) + $(' ' * $spacesOuterLines) + $('/' * $outerMarginLength)
+
+    return ('{0}{2}{1}{2}{0}{2}{2}' -f $outerDividerLine, $middleDividerLine, [System.Environment]::NewLine)
+}
+
+function Get-ModuleParameter {
+    param (
+        [Parameter(Mandatory = $true)]
+        [object] $ParameterData
+    )
+
+    $result = ''
+
+    # description line (optional)
+    if ($ParameterData.description) {
+        $descriptionLine = "@description('" + $ParameterData.description + "')"
+    }
+
+    # todo:
+    # secure (optional)
+    # allowed (optional)
+    # minValue (optional)
+    # maxValue (optional)
+    # minLength (optional)
+    # maxLength (optional)
+    # other?
+
+    # param line (mandatory)
+    $paramLine = 'param ' + $ParameterData.name + ' ' + $ParameterData.type
+
+    if ($ParameterData.default) {
+        $paramLine += ' = ' + $ParameterData.default
+    }
+    # to do: default value depending on type: quotes/no quotes, boolean, arrays (multiline) etc...
+
+    # building and returning the final parameter entry
+    $result = $descriptionLine + [System.Environment]::NewLine + $paramLine + [System.Environment]::NewLine + [System.Environment]::NewLine
+    return $result
+}
+
+function Get-DeploymentResourceFirstLine {
+    param (
+        [Parameter(Mandatory = $true)]
+        [object] $ParameterData
+    )
+    # resource keyVault 'Microsoft.KeyVault/vaults@2021-11-01-preview' = {
+
+    $result = ''
+
+    $result += "@description('" + $ParameterData.description + "')"
+
+    $paramLine = 'param ' + $ParameterData.name + ' ' + $ParameterData.type
+    if ($ParameterData.default) {
+        $paramLine += ' = ' + $ParameterData.default
+    }
+
+    # param location string = resourceGroup().location
+    $result = $descriptionLine + [System.Environment]::NewLine + $paramLine + [System.Environment]::NewLine
+    return $result
+}
+
+function Get-IntentSpaces {
+    param (
+        [Parameter(Mandatory = $false)]
+        [int] $level = 0
+    )
+
+    return ' ' * 2 * $($level + 1)
+}
+
+function Set-ModuleTempalate {
 
     [CmdletBinding(SupportsShouldProcess)]
     param (
@@ -24,8 +104,40 @@
     }
 
     process {
+        ##########################################
+        ##  Create template parameters section  ##
+        ##########################################
+
+        $templateContent = ''
+
+        # Parameters header comment
+        $templateContent += Get-SectionDivider -SectionName 'Parameters'
+
+        # Add parameters
+        foreach ($parameter in $ModuleData.parameters) {
+            $templateContent += Get-ModuleParameter -ParameterData $parameter
+        }
+
+        ###########################################
+        ##  Create template deployments section  ##
+        ###########################################
+
+        # Deployments header comment
+        $templateContent += Get-SectionDivider -SectionName 'Deployments'
+
+        # Deployment resource declaration line
+        # to do
+
+        # Add deployment parameters
+        foreach ($parameter in $ModuleData.parameters) {
+            # to do
+        }
+
+        # Deployment resource finising line
+        # to do
 
 
+        return $templateContent # will be replaced with writing the template file
     }
 
     end {
@@ -33,3 +145,27 @@
     }
 
 }
+
+# using dummy module data for the first draft
+$moduleData = @{
+    parameters = @(
+        @{
+            level         = 0
+            name          = 'sku'
+            type          = 'object'
+            description   = 'Optional. My sample description'
+            allowedValues = @('')
+            required      = $false
+            default       = ''
+        }
+        @{
+            level       = 1
+            name        = 'firewallEnabled'
+            type        = 'boolean'
+            description = 'Optional. My sample description 2'
+            default     = $true
+        }
+    )
+}
+
+Set-ModuleTempalate -ProviderNamespace 'Microsoft.KeyVault' -ResourceType 'vaults' -ModuleData $moduleData
