e9509e4f8b209c7e35ea6a3b1eb9f65ac67d2b84
MrMCake <asehr@hotmail.de>
2022-09-22 16:38:43 +0200

    Added git clone to entry func

--- utilities/tools/REST2CARML/Invoke-REST2CARML.ps1
+++ utilities/tools/REST2CARML/Invoke-REST2CARML.ps1
@@ -15,10 +15,18 @@ Mandatory. The resource type to query the data for
 .PARAMETER IncludePreview
 Mandatory. Include preview API versions
 
+.PARAMETER KeepArtifacts
+Optional. Skip the removal of downloaded/cloned artifacts (e.g. the API-Specs repository). Useful if you want to run the function multiple times in a row.
+
 .EXAMPLE
 Invoke-REST2CARML -ProviderNamespace 'Microsoft.Keyvault' -ResourceType 'vaults'
 
 Generate/Update a CARML module for [Microsoft.Keyvault/vaults]
+
+.EXAMPLE
+Invoke-REST2CARML -ProviderNamespace 'Microsoft.Keyvault' -ResourceType 'vaults' -KeepArtifacts
+
+Generate/Update a CARML module for [Microsoft.Keyvault/vaults] and do not download any downloaded/cloned artifact.
 #>
 function Invoke-REST2CARML {
 
@@ -31,7 +39,10 @@ function Invoke-REST2CARML {
         [string] $ResourceType,
 
         [Parameter(Mandatory = $false)]
-        [switch] $IncludePreview
+        [switch] $IncludePreview,
+
+        [Parameter(Mandatory = $false)]
+        [switch] $KeepArtifacts
     )
 
     begin {
@@ -44,40 +55,81 @@ function Invoke-REST2CARML {
         . (Join-Path $PSScriptRoot 'Set-Module.ps1')
 
         Write-Verbose ('Processing module [{0}/{1}]' -f $ProviderNamespace, $ResourceType) -Verbose
+
+        $initialLocation = (Get-Location).Path
     }
 
     process {
 
-        ###########################
-        ##   Fetch module data   ##
-        ###########################
-        $getPathDataInputObject = @{
-            ProviderNamespace = $ProviderNamespace
-            ResourceType      = $ResourceType
-            IncludePreview    = $IncludePreview
-        }
-        $pathData = Get-ServiceSpecPathData @getPathDataInputObject
-        $moduleData = Resolve-ModuleData -JSONFilePath $pathData.jsonFilePath -JSONKeyPath $pathData.jsonKeyPath
+        #########################################
+        ##   Temp Clone API Specs Repository   ##
+        #########################################
 
-        ###########################################
-        ##   Generate initial module structure   ##
-        ###########################################
-        if ($PSCmdlet.ShouldProcess(('Module [{0}/{1}] structure' -f $ProviderNamespace, $ResourceType), 'Create/Update')) {
-            Set-ModuleFileStructure -ProviderNamespace $ProviderNamespace -ResourceType $ResourceType
+        $repoUrl = 'https://github.com/Azure/azure-rest-api-specs.git'
+        $repoName = Split-Path $repoUrl -LeafBase
+        $tempFolderName = 'temp'
+        $tempFolderPath = Join-Path $PSScriptRoot $tempFolderName
+
+        # Clone repository
+        ## Create temp folder
+        if (-not (Test-Path $tempFolderPath)) {
+            $null = New-Item -Path $tempFolderPath -ItemType 'Directory'
+        }
+        ## Switch to temp folder
+        Set-Location $tempFolderPath
+
+        ## Clone repository into temp folder
+        if (-not (Test-Path (Join-Path $tempFolderPath $repoName))) {
+            git clone git clone --depth=1 --single-branch --branch=main --filter=tree:0 $repoUrl
+        } else {
+            Write-Verbose "Repository [$repoName] already cloned"
         }
 
-        ############################
-        ##   Set module content   ##
-        ############################
-        $moduleTemplateInputObject = @{
-            ProviderNamespace     = $ProviderNamespace
-            ResourceType          = $ResourceType
-            SpecificationFilePath = $pathData.jsonFilePath
-            SpecificationUrl      = $pathData.jsonKeyPath
-            ModuleData            = $moduleData
-        }
-        if ($PSCmdlet.ShouldProcess(('Module [{0}/{1}] files' -f $ProviderNamespace, $ResourceType), 'Create/Update')) {
-            Set-Module @moduleTemplateInputObject
+        Set-Location $initialLocation
+
+        try {
+            ###########################
+            ##   Fetch module data   ##
+            ###########################
+            $getPathDataInputObject = @{
+                ProviderNamespace = $ProviderNamespace
+                ResourceType      = $ResourceType
+                IncludePreview    = $IncludePreview
+            }
+            $pathData = Get-ServiceSpecPathData @getPathDataInputObject
+            $moduleData = Resolve-ModuleData -JSONFilePath $pathData.jsonFilePath -JSONKeyPath $pathData.jsonKeyPath
+
+            ###########################################
+            ##   Generate initial module structure   ##
+            ###########################################
+            if ($PSCmdlet.ShouldProcess(('Module [{0}/{1}] structure' -f $ProviderNamespace, $ResourceType), 'Create/Update')) {
+                Set-ModuleFileStructure -ProviderNamespace $ProviderNamespace -ResourceType $ResourceType
+            }
+
+            ############################
+            ##   Set module content   ##
+            ############################
+            $moduleTemplateInputObject = @{
+                ProviderNamespace     = $ProviderNamespace
+                ResourceType          = $ResourceType
+                SpecificationFilePath = $pathData.jsonFilePath
+                SpecificationUrl      = $pathData.jsonKeyPath
+                ModuleData            = $moduleData
+            }
+            if ($PSCmdlet.ShouldProcess(('Module [{0}/{1}] files' -f $ProviderNamespace, $ResourceType), 'Create/Update')) {
+                Set-Module @moduleTemplateInputObject
+            }
+
+        } catch {
+            throw $_
+        } finally {
+            ############################
+            ##   Remove Artifacts   ##
+            ############################
+            if (-not $KeepArtifacts) {
+                Write-Verbose "Deleting folder [$tempFolderPath]"
+                $null = Remove-Item $tempFolderPath -Recurse -Force
+            }
         }
     }
 
@@ -86,4 +138,4 @@ function Invoke-REST2CARML {
     }
 }
 
-Invoke-REST2CARML -ProviderNamespace 'Microsoft.Storage' -ResourceType 'storageAccounts' -Verbose
+Invoke-REST2CARML -ProviderNamespace 'Microsoft.Storage' -ResourceType 'storageAccounts' -Verbose -KeepArtifacts
