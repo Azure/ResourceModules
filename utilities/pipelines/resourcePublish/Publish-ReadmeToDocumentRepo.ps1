4c8da9a6cfc31f7dda00733f19d18e5f37d517b2
SeSeicht <Sebastian.Seichter@microsoft.com>
2022-09-23 18:59:55 +0200

    update publishing script

--- utilities/pipelines/resourcePublish/Publish-ReadmeToDocumentRepo.ps1
+++ utilities/pipelines/resourcePublish/Publish-ReadmeToDocumentRepo.ps1
@@ -25,9 +25,67 @@ function Publish-ReadmeToDocumentRepo {
         [string] $ReadMeFilePath,
 
         [Parameter(Mandatory)]
-        [string] $ModuleVersion
+        [string] $ModuleVersion,
+
+        [Parameter(Mandatory)]
+        [string] $wikiPath
     )
 
     Set-NewReadmeVersion -Path $ReadMeFilePath -Version $ModuleVersion
 
+    #####
+    Set-Location $wikiPath
+
+    Invoke-Git -Command 'fetch'
+    Invoke-Git -Command 'checkout wikiMaster'
+
+
+
+    ## publishing md to wiki
+    Write-Host '##############################'
+    Write-Host 'Publishing...'
+    Invoke-Git -Command "config user.email 'wikipublisher@carml.local'"
+    Invoke-Git -Command "config user.name 'WIKI-Publisher'"
+    Invoke-Git -Command 'add .'
+    $msg = 'Wiki updated by pipeline'
+    Invoke-Git -Command "commit -m `"$msg`"" -IgnoreExitCode
+    Invoke-Git -Command 'push'
+    <#
+    .SYNOPSIS
+    ##
+
+    .DESCRIPTION
+    Long description
+
+    .PARAMETER ReadMeFilePath
+    Parameter description
+
+    .PARAMETER ModuleVersion
+    Parameter description
+
+    .PARAMETER wikiPath
+    Parameter description
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    General notes
+    #>
+}
+
+function Invoke-Git {
+    param(
+        [Parameter(Mandatory)]
+        [string] $Command,
+        [Parameter(Mandatory = $false)]
+        [switch] $IgnoreExitCode
+    )
+    Write-Host -ForegroundColor Green "`nExecuting: git $Command"
+    $output = Invoke-Expression "git $Command"
+    if ($LASTEXITCODE -gt 0 -and $IgnoreExitCode -eq $false) {
+        Throw "Error Encountered executing: 'git $Command '"
+    } else {
+        Write-Host $output
+    }
 }
