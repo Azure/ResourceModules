e85b75b7e3562a23f5a1417a325e4b1997056a02
Alexander Sehr <ASehr@hotmail.de>
2022-10-11 20:27:40 +0200

    [Modules] Refactored Network Application Gateway to new dependency approach (#1871)
    * Started refactoring
    
    * Finished first version
    
    * Update to latest
    
    * Fixed pip deployment
    
    * sync
    
    * Update to latest
    
    * Update to latest
    
    * Update to latest
    
    * Updated docs
    
    * Update to latest
    
    * Updaed script
    
    * Updated folder default to common.
    
    * Update to latest
    
    * Update to latest
    
    * Update modules/Microsoft.Network/applicationGateways/.test/common/deploy.test.bicep

---
+++ modules/Microsoft.Network/applicationGateways/.test/.scripts/New-Certificate.ps1
@@ -0,0 +1,40 @@
+﻿param(
+    [string] $KeyVaultName,
+    [string] $CertName
+)
+
+$certificate = Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertName -ErrorAction 'Stop'
+
+if (-not $certificate) {
+    $policyInputObject = @{
+        SecretContentType = 'application/x-pkcs12'
+        SubjectName       = 'CN=fabrikam.com'
+        IssuerName        = 'Self'
+        ValidityInMonths  = 12
+        ReuseKeyOnRenewal = $true
+    }
+    $certPolicy = New-AzKeyVaultCertificatePolicy @policyInputObject
+
+    $null = Add-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertName -CertificatePolicy $certPolicy
+    Write-Verbose ('Initiated creation of certificate [{0}] in key vault [{1}]' -f $CertName, $KeyVaultName) -Verbose
+
+    while (-not (Get-AzKeyVaultCertificateOperation -VaultName $KeyVaultName -Name $CertName).Status -eq 'completed') {
+        Write-Verbose 'Waiting 10 seconds for certificate creation' -Verbose
+        Start-Sleep 10
+    }
+
+    Write-Verbose 'Certificate created' -Verbose
+}
+
+$secretId = $certificate.SecretId
+while ([String]::IsNullOrEmpty($secretId)) {
+    Write-Verbose 'Waiting 10 seconds until certificate can be fetched' -Verbose
+    Start-Sleep 10
+    $certificate = Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertName -ErrorAction 'Stop'
+    $secretId = $certificate.SecretId
+}
+
+# Write into Deployment Script output stream
+$DeploymentScriptOutputs = @{
+    secretUrl = $secretId
+}
