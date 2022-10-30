571cb6ff43203804b0201f225ca57274140eb18d
MrMCake <asehr@hotmail.de>
2022-10-30 12:20:35 +0100

    Update to latest

---
+++ utilities/tools/REST2CARML/private/module/Read-DeclarationBlock.ps1
@@ -0,0 +1,101 @@
+﻿<#
+.SYNOPSIS
+Find all instances of a Bicep delcaration block (e.g. 'param') in the given template file
+
+.DESCRIPTION
+Find all instances of a Bicep delcaration block (e.g. 'param') in the given template file. Returns an array of all ocurrences, including their content, start- & end-index
+
+.PARAMETER DeclarationContent
+Mandatory. The Bicep template content to search in
+
+.PARAMETER DeclarationType
+Mandatory. The declaration type to search for. Can be 'param', 'var', 'resource', 'module', or 'output'
+
+.EXAMPLE
+Read-DeclarationBlock -DeclarationContent @('targetScope = 'subscription', '', '@description('Some Description'), param description string, '', (..)) -DeclarationType 'param
+
+Get all 'param' declaration blocks of the given declaration content.
+#>
+function Read-DeclarationBlock {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [object[]] $DeclarationContent,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('param', 'var', 'resource', 'module', 'output')]
+        [string] $DeclarationType
+    )
+
+    # Any keyword when moving from the current line up to indicate that it's a new declaration
+    $newLogicIdentifiersUp = @(
+        '^targetScope.+$',
+        '^param .+$',
+        '^var .+$',
+        '^resource .+$',
+        '^module .+$',
+        '^output .+$',
+        '^//.*$'
+        '^\s*$'
+    )
+    # Any keyword when moving from the current line down to indicate that it's a new declaration
+    $newLogicIdentifiersDown = @(
+        '^param .+$',
+        '^var .+$',
+        '^resource .+$',
+        '^module .+$',
+        '^output .+$',
+        '^//.*$',
+        '^@.+$',
+        '^\s*$'
+    )
+
+    $existingBlocks = @()
+
+    $declarationIndexes = @()
+    for ($index = 0; $index -lt $DeclarationContent.Count; $index++) {
+        if ($DeclarationContent[$index] -like "$DeclarationType *") {
+            $declarationIndexes += $index
+        }
+    }
+
+    foreach ($declarationIndex in $declarationIndexes) {
+        switch ($DeclarationType) {
+            { $PSItem -in @('param', 'output') } {
+                # Let's go 'up' until the declarations end
+                $declarationStartIndex = $declarationIndex
+                while ($DeclarationContent[$declarationStartIndex] -eq $DeclarationContent[$declarationIndex] -or (($newLogicIdentifiersUp | Where-Object { $DeclarationContent[$declarationStartIndex] -match $_ }).Count -eq 0 -and $declarationStartIndex -ne 0)) {
+                    $declarationStartIndex--
+                }
+                # Logic always counts one too far
+                $declarationStartIndex++
+
+                # The declaration line is always the last line of the block
+                $declarationEndIndex = $declarationIndex
+                break
+            }
+            { $PSItem -in @('var', 'resource', 'module') } {
+                # The declaration line is always the first line of the block
+                $declarationStartIndex = $declarationIndex
+
+                # Let's go 'down' until the var declarations end
+                $declarationEndIndex = $declarationIndex
+                while ($DeclarationContent[$declarationEndIndex] -eq $DeclarationContent[$declarationIndex] -or (($newLogicIdentifiersDown | Where-Object { $DeclarationContent[$declarationEndIndex] -match $_ }).Count -eq 0 -and $declarationEndIndex -ne $DeclarationContent.Count)) {
+                    $declarationEndIndex++
+                }
+                # Logic always counts one too far
+                $declarationEndIndex--
+                break
+            }
+        }
+
+        $existingBlocks += @{
+            content    = $templateContent[$declarationStartIndex .. $declarationEndIndex]
+            startIndex = $declarationStartIndex
+            endIndex   = $declarationEndIndex
+        }
+    }
+
+    return $existingBlocks
+}
--- utilities/tools/REST2CARML/private/module/Resolve-ExistingTemplateContent.ps1
+++ utilities/tools/REST2CARML/private/module/Resolve-ExistingTemplateContent.ps1
@@ -12,136 +12,36 @@
 
     $templateContent = Get-Content -Path $TemplateFilePath
 
-    # Any keyword when moving from the current line up to indicate that it's a new declaration
-    $newLogicIdentifiersUp = @(
-        '^targetScope.+$',
-        '^param .+$',
-        '^var .+$',
-        '^resource .+$',
-        '^module .+$',
-        '^output .+$',
-        '^//.*$'
-        '^\s*$'
-    )
-    # Any keyword when moving from the current line down to indicate that it's a new declaration
-    $newLogicIdentifiersDown = @(
-        '^param .+$',
-        '^var .+$',
-        '^resource .+$',
-        '^module .+$',
-        '^output .+$',
-        '^//.*$',
-        '^@.+$',
-        '^\s*$'
-    )
-
     ############################
     ##   Extract Parameters   ##
     ############################
-    $existingParameterBlocks = @()
-
-    $paramIndexes = @()
-    for ($index = 0; $index -lt $templateContent.Count; $index++) {
-        if ($templateContent[$index] -like 'param *') {
-            $paramIndexes += $index
-        }
-    }
-
-    foreach ($paramIndex in $paramIndexes) {
-
-        # Let's go 'up' until the param declarations end
-        $paramStartIndex = $paramIndex
-        while ($templateContent[$paramStartIndex] -eq $templateContent[$paramIndex] -or (($newLogicIdentifiersUp | Where-Object { $templateContent[$paramStartIndex] -match $_ }).Count -eq 0 -and $paramStartIndex -ne 0)) {
-            $paramStartIndex--
-        }
-        # Logic always counts one too far
-        $paramStartIndex++
-
-        # The param line is always the last line of a param
-        $paramEndIndex = $paramIndex
-
-        $existingParameterBlocks += @{
-            content    = $templateContent[$paramStartIndex .. $paramEndIndex]
-            startIndex = $paramStartIndex
-            endIndex   = $paramEndIndex
-        }
-    }
+    $existingParameterBlocks = Read-DeclarationBlock -DeclarationContent $templateContent -DeclarationType 'param'
 
     ###########################
     ##   Extract Variables   ##
     ###########################
-    $existingVariableBlocks = @()
-
-    $varIndexes = @()
-    for ($index = 0; $index -lt $templateContent.Count; $index++) {
-        if ($templateContent[$index] -like 'var *') {
-            $varIndexes += $index
-        }
-    }
-
-    foreach ($varIndex in $varIndexes) {
-
-        # The var line is always the first line of a var
-        $varStartIndex = $varIndex
-
-
-        # Let's go 'down' until the var declarations end
-        $varEndIndex = $varIndex
-        while ($templateContent[$varEndIndex] -eq $templateContent[$varIndex] -or (($newLogicIdentifiersDown | Where-Object { $templateContent[$varEndIndex] -match $_ }).Count -eq 0 -and $varEndIndex -ne $templateContent.Count)) {
-            $varEndIndex++
-        }
-        # Logic always counts one too far
-        $varEndIndex--
-
-        $existingVariableBlocks += @{
-            content    = $templateContent[$varStartIndex .. $varEndIndex]
-            startIndex = $varStartIndex
-            endIndex   = $varEndIndex
-        }
-    }
+    $existingVariableBlocks = Read-DeclarationBlock -DeclarationContent $templateContent -DeclarationType 'var'
 
     ###########################
     ##   Extract Resources   ##
     ###########################
-    $existingdeploymentBlock = @()
+    $existingResourceBlocks = Read-DeclarationBlock -DeclarationContent $templateContent -DeclarationType 'resource'
 
+    #########################
+    ##   Extract Modules   ##
+    #########################
+    $existingModuleBlocks = Read-DeclarationBlock -DeclarationContent $templateContent -DeclarationType 'module'
 
     #########################
     ##   Extract Outputs   ##
     #########################
-    $existingOutputBlocks = @()
-
-    $outputIndexes = @()
-    for ($index = 0; $index -lt $templateContent.Count; $index++) {
-        if ($templateContent[$index] -like 'output *') {
-            $outputIndexes += $index
-        }
-    }
-
-    foreach ($outputIndex in $outputIndexes) {
-
-        # Let's go 'up' until the output declarations end
-        $outputStartIndex = $outputIndex
-        while ($templateContent[$outputStartIndex] -eq $templateContent[$outputIndex] -or (($newLogicIdentifiersUp | Where-Object { $templateContent[$outputStartIndex] -match $_ }).Count -eq 0 -and $outputStartIndex -ne 0)) {
-            $outputStartIndex--
-        }
-        # Logic always counts one too far
-        $outputStartIndex++
-
-        # The output line is always the last line of a output
-        $outputEndIndex = $outputIndex
-
-        $existingOutputBlocks += @{
-            content    = $templateContent[$outputStartIndex .. $outputEndIndex]
-            startIndex = $outputStartIndex
-            endIndex   = $outputEndIndex
-        }
-    }
+    $existingOutputBlocks = Read-DeclarationBlock -DeclarationContent $templateContent -DeclarationType 'output'
 
     return @{
         parameters = $existingParameterBlocks
         variables  = $existingVariableBlocks
-        resources  = $existingVariableBlocks
+        resources  = $existingResourceBlocks
+        modules    = $existingModuleBlocks
         outputs    = $existingOutputBlocks
     }
 }
