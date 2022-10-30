b7bc8fbd1889912bbcf2042ef8bb442509ee7c56
MrMCake <asehr@hotmail.de>
2022-10-30 09:11:19 +0100

    Update to latest

--- utilities/tools/REST2CARML/private/module/Resolve-ExistingTemplateContent.ps1
+++ utilities/tools/REST2CARML/private/module/Resolve-ExistingTemplateContent.ps1
@@ -10,10 +10,68 @@
         return
     }
 
-    $existingTemplateContent = Get-Content -Path $TemplateFilePath
+    $templateContent = Get-Content -Path $TemplateFilePath
 
+    # Any keyword when moving from the current line up to indicate that it's a new declaration
+    $newLogicIdentifiersUp = @(
+        '^targetScope.+$',
+        '^param .+$',
+        '^var .+$',
+        '^resource .+$',
+        '^module .+$',
+        '^output .+$',
+        '^//.*$'
+        '^\s*$'
+    )
+    # Any keyword when moving from the current line down to indicate that it's a new declaration
+    $newLogicIdentifiersDown = @(
+        '^param .+$',
+        '^var .+$',
+        '^resource .+$',
+        '^module .+$',
+        '^output .+$',
+        '^//.*$',
+        '^@.+$',
+        '^\s+$'
+    )
+
+    ############################
+    ##   Extract Parameters   ##
+    ############################
     $existingParametersBlock = @()
+
+    $paramIndexes = @()
+    for ($index = 0; $index -lt $templateContent.Count; $index++) {
+        if ($templateContent[$index] -like 'param *') {
+            $paramIndexes += $index
+        }
+    }
+
+    foreach ($paramIndex in $paramIndexes) {
+
+        # Let's go 'up' until the param declarations end
+        $paramStartIndex = $paramIndex
+        while ($templateContent[$paramStartIndex] -eq $templateContent[$paramIndex] -or (($newLogicIdentifiersUp | Where-Object { $templateContent[$paramStartIndex] -match $_ }).Count -eq 0 -and $paramStartIndex -ne 0)) {
+            $paramStartIndex--
+        }
+        # Logic always counts one too far
+        $paramStartIndex++
+
+        # The param line is always the last line of a param
+        $paramEndIndex = $paramIndex
+
+        $existingParametersBlock += @{
+            content    = $templateContent[$paramStartIndex .. $paramIndex]
+            startIndex = $paramStartIndex
+            endIndex   = $paramEndIndex
+        }
+    }
+
+    ###########################
+    ##   Extract Variables   ##
+    ###########################
     $existingVariablesBlock = @()
     $existingdeploymentBlock = @()
     $existingOutputsBlock = @()
 }
+Resolve-ExistingTemplateContent -TemplateFilePath 'C:\dev\ip\Azure-ResourceModules\ResourceModules\modules\Microsoft.Storage\storageAccounts\deploy.bicep'
