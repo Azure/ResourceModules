3c3cd9e208637f353b36a628a85b8dd014e70c50
MrMCake <asehr@hotmail.de>
2022-11-06 12:13:57 +0100

    Updated docs

--- utilities/tools/REST2CARML/private/module/Get-TemplateOutputContent.ps1
+++ utilities/tools/REST2CARML/private/module/Get-TemplateOutputContent.ps1
@@ -1,4 +1,76 @@
-﻿function Get-TemplateOutputContent {
+﻿<#
+.SYNOPSIS
+Get the formatted content for the template's 'outputs' section
+
+.DESCRIPTION
+Get the formatted content for the template's 'outputs' section. For the primary resource, template content of any pre-existing template takes precedence over new content.
+
+.PARAMETER ResourceType
+Mandatory. The resource type without the provider namespace (e.g., 'storageAccounts')
+
+.PARAMETER ResourceTypeSingular
+Optional. The 'singular' version of the resource type. For example 'container' instead of 'containers'.
+
+.PARAMETER TargetScope
+Mandatory. The scope of the target template (e.g., 'resourceGroup', 'subscription', etc.)
+
+.PARAMETER ModuleData
+Mandatory. The module data to fetch the data for this section from & then format it propertly for the template.
+
+Expects an array with objects like:
+Name                           Value
+----                           -----
+parameters                     {name, identity, type, properties…}
+outputs                        {}
+additionalFiles                {}
+modules                        {}
+variables                      {diagnosticsMetrics, diagnosticsLogs}
+resources                      {privateCloud_diagnosticSettings, privateCloud_lock}
+isSingleton                    False
+additionalParameters           {diagnosticLogsRetentionInDays, diagnosticStorageAccountId, diagnosticWorkspaceId, diagnosticEventHubAuthorizationRuleId…}
+
+.PARAMETER ExistingTemplateContent
+Optional. The prepared content of an existing template, if any.
+
+Expects an array with objects like:
+
+Name                           Value
+----                           -----
+modules                        {privateCloud_cloudLinks, privateCloud_hcxEnterpriseSites, privateCloud_authorizations, privateCloud_scriptExecutions…}
+variables                      {diagnosticsMetrics, diagnosticsLogs, enableReferencedModulesTelemetry}
+parameters                     {name, sku, addons, authorizations…}
+outputs                        {name, resourceId, resourceGroupName}
+resources                      {defaultTelemetry, privateCloud, privateCloud_diagnosticSettings, privateCloud_lock}
+
+.EXAMPLE
+$contentInputObject = @{
+    ResourceType            = 'privateClouds/clusters/datastores'
+    ResourceTypeSingular    = 'datastore'
+    TargetScope             = 'resourceGroup'
+    ModuleData              = @(@{...}, (...))
+    ExistingTemplateContent = @(@{...}, (...))
+}
+Get-TemplateOutputContent @contentInputObject
+
+Get the formatted template content for resource type 'Microsoft.AVS/privateClouds/clusters/datastores' based on the given data - including an existing template's data. The  output looks something like:
+
+```bicep
+// =========== //
+//   Outputs   //
+// =========== //
+
+@description('The name of the datastore.')
+output name string = datastore.name
+
+@description('The resource ID of the datastore.')
+output resourceId string = datastore.id
+
+@description('The name of the resource group the datastore was created in.')
+output resourceGroupName string = resourceGroup().name
+(...)
+```
+#>
+function Get-TemplateOutputContent {
 
     [CmdletBinding()]
     param (
