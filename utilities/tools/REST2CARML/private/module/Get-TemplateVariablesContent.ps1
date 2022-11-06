25c98807bbbf668fb77bf57a364c714076211811
MrMCake <asehr@hotmail.de>
2022-11-06 10:55:12 +0100

    Added docs

--- utilities/tools/REST2CARML/private/module/Get-TemplateVariablesContent.ps1
+++ utilities/tools/REST2CARML/private/module/Get-TemplateVariablesContent.ps1
@@ -1,4 +1,53 @@
-﻿function Get-TemplateVariablesContent {
+﻿<#
+.SYNOPSIS
+Get the formatted content for the template's 'variables' section
+
+.DESCRIPTION
+Get the formatted content for the template's 'variables' section. 'Existing' template content taxes precedence over new variables.
+
+.PARAMETER ModuleData
+Mandatory. The module data to fetch the data for this section from & then format it propertly for the template.
+
+Expects an array with objects like:
+Name                           Value
+----                           -----
+parameters                     {name, identity, type, properties…}
+outputs                        {}
+additionalFiles                {}
+modules                        {}
+variables                      {diagnosticsMetrics, diagnosticsLogs}
+resources                      {privateCloud_diagnosticSettings, privateCloud_lock}
+isSingleton                    False
+additionalParameters           {diagnosticLogsRetentionInDays, diagnosticStorageAccountId, diagnosticWorkspaceId, diagnosticEventHubAuthorizationRuleId…}
+
+.PARAMETER ExistingTemplateContent
+Optional. The prepared content of an existing template, if any.
+
+Expects an array with objects like:
+
+Name                           Value
+----                           -----
+modules                        {privateCloud_cloudLinks, privateCloud_hcxEnterpriseSites, privateCloud_authorizations, privateCloud_scriptExecutions…}
+variables                      {diagnosticsMetrics, diagnosticsLogs, enableReferencedModulesTelemetry}
+parameters                     {name, sku, addons, authorizations…}
+outputs                        {name, resourceId, resourceGroupName}
+resources                      {defaultTelemetry, privateCloud, privateCloud_diagnosticSettings, privateCloud_lock}
+
+.EXAMPLE
+Get-TemplateVariablesContent -ModuleData @(@{ variables = @(@{ name = 'abc'; content = @( var abc = (...)) }; (...))}, (...)) -ExistingTemplateContent @(@{ variables = @(@{ name = 'abc'; content = @( var abc = (...))}, (...))}, (...))
+
+Generate the variables content for the above example containing at least the 'abc' variable. Would result in an output like
+
+```Bicep
+// ============= //
+//   Variables   //
+// ============= //
+
+var abc = (...)
+(...)
+```
+#>
+function Get-TemplateVariablesContent {
 
     [CmdletBinding()]
     param (
