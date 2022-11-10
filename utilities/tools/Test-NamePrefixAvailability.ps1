f13ae00365520c045c7b6c7deff20aff232cea15
Felix Borst <felix.borst@microsoft.com>
2022-11-10 17:47:41 +0100

    test

--- utilities/tools/Test-NamePrefixAvailability.ps1
+++ utilities/tools/Test-NamePrefixAvailability.ps1
@@ -121,89 +121,79 @@ function Test-NamePrefixAvailability {
         $containerRegistryNames = $containerRegistryNames | Select-Object -Unique
         $keyVaultNames = $keyVaultNames | Select-Object -Unique
 
-        try {
+        # Extract Parameter Names
+        # -----------------------
+        $subscriptionId = (Get-AzContext).Subscription.Id
 
-            # Extract Parameter Names
-            # -----------------------
-            $subscriptionId = (Get-AzContext).Subscription.Id
-
-            # Storage
-            Write-Host "`nCheck Storage Accounts" -ForegroundColor 'Cyan'
-            Write-Host '======================' -ForegroundColor 'Cyan'
-            foreach ($storageAccountName in $storageAccountNames) {
-                $path = '/subscriptions/{0}/providers/Microsoft.Storage/checkNameAvailability?api-version=2021-04-01' -f $subscriptionId
-                $requestInputObject = @{
-                    Method  = 'POST'
-                    Path    = $path
-                    Payload = @{
-                        name = $storageAccountName
-                        type = 'Microsoft.Storage/storageAccounts'
-                    } | ConvertTo-Json
-                }
-                $response = ((Invoke-AzRestMethod @requestInputObject).Content | ConvertFrom-Json)
-                if ($response.nameAvailable) { Write-Host "=> The storage account name [$storageAccountName] is currently available" -ForegroundColor 'Green' }
-                else {
-                    Write-Warning "=> Warning: The storage account name [$storageAccountName] is not available. Please try a different prefix."
-                    $prefixAvailable = $false
-                }
+        # Storage
+        Write-Host "`nCheck Storage Accounts" -ForegroundColor 'Cyan'
+        Write-Host '======================' -ForegroundColor 'Cyan'
+        foreach ($storageAccountName in $storageAccountNames) {
+            $path = '/subscriptions/{0}/providers/Microsoft.Storage/checkNameAvailability?api-version=2021-04-01' -f $subscriptionId
+            $requestInputObject = @{
+                Method  = 'POST'
+                Path    = $path
+                Payload = @{
+                    name = $storageAccountName
+                    type = 'Microsoft.Storage/storageAccounts'
+                } | ConvertTo-Json
             }
-
-            # Key Vault
-            Write-Host "`nChecking Key Vaults" -ForegroundColor 'Cyan'
-            Write-Host '===================' -ForegroundColor 'Cyan'
-            foreach ($keyVaultName in $keyVaultNames) {
-                $path = '/subscriptions/{0}/providers/Microsoft.KeyVault/checkNameAvailability?api-version=2021-10-01' -f $subscriptionId
-                $requestInputObject = @{
-                    Method  = 'POST'
-                    Path    = $path
-                    Payload = @{
-                        name = $keyVaultName
-                        type = 'Microsoft.KeyVault/vaults'
-                    } | ConvertTo-Json
-                }
-                $response = ((Invoke-AzRestMethod @requestInputObject).Content | ConvertFrom-Json)
-                if ($response.nameAvailable) { Write-Host "=> The key vault name [$keyVaultName] is currently available" -ForegroundColor 'Green' }
-                else {
-                    Write-Warning "=> Warning: The key vault name [$keyVaultName] is not available. Please try a different prefix."
-                    $prefixAvailable = $false
-                }
+            $response = ((Invoke-AzRestMethod @requestInputObject).Content | ConvertFrom-Json)
+            if ($response.nameAvailable) { Write-Host "=> The storage account name [$storageAccountName] is currently available" -ForegroundColor 'Green' }
+            else {
+                Write-Warning "=> Warning: The storage account name [$storageAccountName] is not available. Please try a different prefix."
+                $prefixAvailable = $false
             }
+        }
 
-            # Azure Container Registry
-            Write-Host "`nCheck Azure Container Registies" -ForegroundColor 'Cyan'
-            Write-Host '==============================' -ForegroundColor 'Cyan'
-            foreach ($acrName in $acrNames) {
-                $path = '/subscriptions/{0}/providers/Microsoft.ContainerRegistry/checkNameAvailability?api-version=2019-05-01' -f $subscriptionId
-                $requestInputObject = @{
-                    Method  = 'POST'
-                    Path    = $path
-                    Payload = @{
-                        name = $acrName
-                        type = 'Microsoft.ContainerRegistry/registries'
-                    } | ConvertTo-Json
-                }
-                $response = ((Invoke-AzRestMethod @requestInputObject).Content | ConvertFrom-Json)
-                if ($response.nameAvailable) { Write-Host "=> The Azure container registry name [$acrName] is currently available" -ForegroundColor 'Green' }
-                else {
-                    Write-Warning "=> Warning: The Azure container registry name [$acrName] is not available. Please try a different prefix."
-                    $prefixAvailable = $false
-                }
+        # Key Vault
+        Write-Host "`nChecking Key Vaults" -ForegroundColor 'Cyan'
+        Write-Host '===================' -ForegroundColor 'Cyan'
+        foreach ($keyVaultName in $keyVaultNames) {
+            $path = '/subscriptions/{0}/providers/Microsoft.KeyVault/checkNameAvailability?api-version=2021-10-01' -f $subscriptionId
+            $requestInputObject = @{
+                Method  = 'POST'
+                Path    = $path
+                Payload = @{
+                    name = $keyVaultName
+                    type = 'Microsoft.KeyVault/vaults'
+                } | ConvertTo-Json
             }
+            $response = ((Invoke-AzRestMethod @requestInputObject).Content | ConvertFrom-Json)
+            if ($response.nameAvailable) { Write-Host "=> The key vault name [$keyVaultName] is currently available" -ForegroundColor 'Green' }
+            else {
+                Write-Warning "=> Warning: The key vault name [$keyVaultName] is not available. Please try a different prefix."
+                $prefixAvailable = $false
+            }
+        }
 
-            Write-Host "`nRESULT" -ForegroundColor 'Cyan'
-            Write-Host '======' -ForegroundColor 'Cyan'
-            if (-not $prefixAvailable) {
-                Write-Error "=> Prefix [$namePrefix] is not available for all resources. Please try a different one."
-            } else {
-                Write-Host "=> Prefix [$namePrefix] is available for all resources." -ForegroundColor 'Green'
+        # Azure Container Registry
+        Write-Host "`nCheck Azure Container Registies" -ForegroundColor 'Cyan'
+        Write-Host '==============================' -ForegroundColor 'Cyan'
+        foreach ($acrName in $acrNames) {
+            $path = '/subscriptions/{0}/providers/Microsoft.ContainerRegistry/checkNameAvailability?api-version=2019-05-01' -f $subscriptionId
+            $requestInputObject = @{
+                Method  = 'POST'
+                Path    = $path
+                Payload = @{
+                    name = $acrName
+                    type = 'Microsoft.ContainerRegistry/registries'
+                } | ConvertTo-Json
             }
-        } catch {
-            Write-Error $_
-        } finally {
-            # Restore parameter files
-            # -----------------------
-            Write-Verbose 'Restoring Tokens'
-            $null = Convert-TokensInFileList @tokenConfiguration -SwapValueWithName $true
+            $response = ((Invoke-AzRestMethod @requestInputObject).Content | ConvertFrom-Json)
+            if ($response.nameAvailable) { Write-Host "=> The Azure container registry name [$acrName] is currently available" -ForegroundColor 'Green' }
+            else {
+                Write-Warning "=> Warning: The Azure container registry name [$acrName] is not available. Please try a different prefix."
+                $prefixAvailable = $false
+            }
+        }
+
+        Write-Host "`nRESULT" -ForegroundColor 'Cyan'
+        Write-Host '======' -ForegroundColor 'Cyan'
+        if (-not $prefixAvailable) {
+            Write-Error "=> Prefix [$namePrefix] is not available for all resources. Please try a different one."
+        } else {
+            Write-Host "=> Prefix [$namePrefix] is available for all resources." -ForegroundColor 'Green'
         }
     }
 
