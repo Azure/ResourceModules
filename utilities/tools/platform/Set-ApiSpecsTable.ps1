de16689fb876b09687229cf494c71ea9a59b99a4
AlexanderSehr <asehr@hotmail.de>
2022-11-20 14:27:07 +0100

    Added IP to generate API specs file in ADO

---
+++ utilities/tools/platform/Set-ApiSpecsTable.ps1
@@ -0,0 +1,32 @@
+﻿function Set-ApiSpecsTable {
+
+    [CmdletBinding(SupportsShouldProcess = $true)]
+    param (
+        [Parameter(Mandatory = $false)]
+        [string] $SpecsFilePath = (Join-Path (Split-Path (Split-Path $PSScriptRoot -Parent)) 'src' 'apiSpecsList.json')
+    )
+
+    if (-not (Get-Module 'AzureAPICrawler' -ListAvailable)) {
+        if ($PSCmdlet.ShouldProcess("Module 'AzureAPICrawler with version [0.1.2]'", 'Install')) {
+            $null = Install-Module 'AzureAPICrawler' -Scope 'CurrentUser' -Repository 'PSGallery' -RequiredVersion '0.1.2' -Force
+        }
+    }
+
+    $null = Import-Module 'AzureAPICrawler'
+
+
+    if (-not (Test-Path $SpecsFilePath)) {
+        if ($PSCmdlet.ShouldProcess('API Specs file [apiSpecsList.json]', 'Create')) {
+            Write-Verbose 'Generating new API Specs file'
+            $null = New-Item -Path $SpecsFilePath -Force
+        }
+    }
+
+    $res = Get-AzureApiSpecsVersionList -IncludePreview -Verbose
+
+    $fileContent = $res | ConvertTo-Json
+
+    if ($PSCmdlet.ShouldProcess('API Specs file [apiSpecsList.json]', 'Update')) {
+        $null = Set-Item -Path $SpecsFilePath -Value $fileContent -Force
+    }
+}
