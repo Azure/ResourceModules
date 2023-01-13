3678481b4d24804e5152eb6321a97a44b8495c11
AlexanderSehr <asehr@hotmail.de>
2023-01-13 13:11:08 +0100

    Removed TEMP PS Deployment

--- utilities/pipelines/sharedScripts/Set-EnvironmentOnAgent.ps1
+++ utilities/pipelines/sharedScripts/Set-EnvironmentOnAgent.ps1
@@ -248,21 +248,4 @@ function Set-EnvironmentOnAgent {
     }
 
     Write-Verbose ('Install-CustomModule end') -Verbose
-
-    #####################################
-    ##  TEMP PowerShell installation   ##
-    #####################################
-
-    # Update the list of packages
-    sudo apt-get update
-    # Install pre-requisite packages.
-    sudo apt-get install -y wget apt-transport-https software-properties-common
-    # Download the Microsoft repository GPG keys
-    wget -q "https://packages.microsoft.com/config/ubuntu/`$(lsb_release -rs)/packages-microsoft-prod.deb"
-    # Register the Microsoft repository GPG keys
-    sudo dpkg -i packages-microsoft-prod.deb
-    # Update the list of packages after we added packages.microsoft.com
-    sudo apt-get update
-    # Install PowerShell
-    sudo apt-get install -y powershell
 }
