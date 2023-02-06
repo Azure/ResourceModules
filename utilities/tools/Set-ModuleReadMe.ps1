140b02735ac2610fef2febbdfd3737635e98fa35
Marius Storhaug <marstor@hotmail.com>
2023-02-06 12:40:01 +0100

    readded new condition

--- utilities/tools/Set-ModuleReadMe.ps1
+++ utilities/tools/Set-ModuleReadMe.ps1
@@ -1566,8 +1566,8 @@ function Set-ModuleReadMe {
         $readMeFileContent = Set-CrossReferencesSection @inputObject
     }
 
-    $isTopLevelModule = $fullModuleIdentifier.Split('/').Count -eq 2 # <provider>/<resourceType>
-    if ($SectionsToRefresh -contains 'Deployment examples' -and $isTopLevelModule) {
+    $hasTests = Get-ChildItem -Path (Join-Path -Path $moduleRoot -ChildPath '.test') -Recurse -Filter 'deploy.test.bicep'
+    if ($SectionsToRefresh -contains 'Deployment examples' -and $hasTests) {
         # Handle [Deployment examples] section
         # ===================================
         $inputObject = @{
