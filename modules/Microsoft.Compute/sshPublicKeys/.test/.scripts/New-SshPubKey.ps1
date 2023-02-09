0ba1330b8cd12f8118c17e63009c62a74af23913
Shawn Meyer <shawn.meyer@microsoft.com>
2023-02-08 23:20:39 -0500

    add sshPublicKey Module

---
+++ modules/Microsoft.Compute/sshPublicKeys/.test/.scripts/New-SshPubKey.ps1
@@ -0,0 +1,9 @@
+﻿param(
+    [string]$resourceGroupName,
+    [string]$keyName
+)
+$publicKey = (New-AzSshKey -ResourceGroupName $resourceGroupName -Name $keyName -WarningAction SilentlyContinue).publicKey
+Remove-AzSshKey -ResourceGroupName $resourceGroupName -Name $keyName
+Write-Output $publicKey
+$DeploymentScriptOutputs = @{}
+$DeploymentScriptOutputs['pubKey'] = $publicKey
