9e35804230fb343a8a28d52ceca7073046f514ff
Erika Gressi <erikag@microsoft.com>
2023-02-17 16:19:20 +0100

    test path

--- utilities/pipelines/PSRuleValidation/Set-PSRuleGitHubOutput.ps1
+++ utilities/pipelines/PSRuleValidation/Set-PSRuleGitHubOutput.ps1
@@ -41,116 +41,122 @@ function Set-PSRuleGitHubOutput {
     # Import CSV output and filter by results #
     ###########################################
 
-    $results = Import-Csv -Path $inputFilePath
-
-    $passedRules = @()
-    $failedRules = @()
-
-    $passedRules += $results | Where-Object { $_.Outcome -EQ 'Pass' }
-    $failedRules += $results | Where-Object { $_.Outcome -EQ 'Fail' }
-
-    ######################
-    # Set output content #
-    ######################
-
-    # Header
-    $header = [System.Collections.ArrayList]@(
-        '# PSRule pre-flight validation summary ',
-        ''
-    )
-    Out-File -FilePath $outputFilePath -NoClobber -InputObject $header
-
-    if ($failedRules.Count -eq 0) {
-        # No failure content
-        $noFailuresContent = ('## :rocket: All [{0}] rules passed, YAY! :rocket:' -f $results.Count)
-        Out-File -FilePath $outputFilePath -Append -NoClobber -InputObject $noFailuresContent
+    if (-not (Test-Path $inputFilePath)) {
+        Write-Warning "Input File [$inputFilePath] not found"
+        return ''
     } else {
-        # Failure content
 
-        ## Header table
-        $headerTable = [System.Collections.ArrayList]@(
-            '| Total No. of Processed Rules| Passed Rules :white_check_mark: | Failed Rules :x: |',
-            '| :-- | :-- | :-- |'
+        $results = Import-Csv -Path $inputFilePath
+
+        $passedRules = @()
+        $failedRules = @()
+
+        $passedRules += $results | Where-Object { $_.Outcome -EQ 'Pass' }
+        $failedRules += $results | Where-Object { $_.Outcome -EQ 'Fail' }
+
+        ######################
+        # Set output content #
+        ######################
+
+        # Header
+        $header = [System.Collections.ArrayList]@(
+            '# PSRule pre-flight validation summary ',
+            ''
+        )
+        Out-File -FilePath $outputFilePath -NoClobber -InputObject $header
+
+        if ($failedRules.Count -eq 0) {
+            # No failure content
+            $noFailuresContent = ('## :rocket: All [{0}] rules passed, YAY! :rocket:' -f $results.Count)
+            Out-File -FilePath $outputFilePath -Append -NoClobber -InputObject $noFailuresContent
+        } else {
+            # Failure content
+
+            ## Header table
+            $headerTable = [System.Collections.ArrayList]@(
+                '| Total No. of Processed Rules| Passed Rules :white_check_mark: | Failed Rules :x: |',
+                '| :-- | :-- | :-- |'
             ('| {0} | {1} | {2} |' -f $results.Count, $passedRules.Count , $failedRules.Count),
-            ''
-        )
-        Out-File -FilePath $outputFilePath -Append -NoClobber -InputObject $headerTable
+                ''
+            )
+            Out-File -FilePath $outputFilePath -Append -NoClobber -InputObject $headerTable
 
-        ## List of failed rules
-        $failContent = [System.Collections.ArrayList]@(
-            '',
-            '<details>',
-            '<summary>List of Failed Rules</summary>',
-            '',
-            '## Failed Rules',
-            '',
-            '| RuleName | TargetName |  Synopsis |',
-            '| :-- | :-- | :-- |'
-        )
-        foreach ($content in $failedRules ) {
-            # Shorten the target name for deployment resoure type
-            if ($content.TargetType -eq 'Microsoft.Resources/deployments') {
-                $content.TargetName = $content.TargetName.replace('/home/runner/work/ResourceModules/ResourceModules/modules/', '')
-            }
+            ## List of failed rules
+            $failContent = [System.Collections.ArrayList]@(
+                '',
+                '<details>',
+                '<summary>List of Failed Rules</summary>',
+                '',
+                '## Failed Rules',
+                '',
+                '| RuleName | TargetName |  Synopsis |',
+                '| :-- | :-- | :-- |'
+            )
+            foreach ($content in $failedRules ) {
+                # Shorten the target name for deployment resoure type
+                if ($content.TargetType -eq 'Microsoft.Resources/deployments') {
+                    $content.TargetName = $content.TargetName.replace('/home/runner/work/ResourceModules/ResourceModules/modules/', '')
+                }
 
-            # Build hyperlinks to PSRule documentation for the rules
-            $TemplatesBaseUrl = 'https://azure.github.io/PSRule.Rules.Azure/en/rules'
-            try {
-                $PSRuleReferenceUrl = '{0}/{1}' -f $TemplatesBaseUrl, $content.RuleName
-                $null = Invoke-WebRequest -Uri $PSRuleReferenceUrl
-                $resourceLink = '[{0}]({1})' -f $content.RuleName, $PSRuleReferenceUrl
-            } catch {
-                Write-Warning ('Unable to build url for rule [{0}]' -f $content.RuleName)
-                $resourceLink = $content.RuleName
+                # Build hyperlinks to PSRule documentation for the rules
+                $TemplatesBaseUrl = 'https://azure.github.io/PSRule.Rules.Azure/en/rules'
+                try {
+                    $PSRuleReferenceUrl = '{0}/{1}' -f $TemplatesBaseUrl, $content.RuleName
+                    $null = Invoke-WebRequest -Uri $PSRuleReferenceUrl
+                    $resourceLink = '[{0}]({1})' -f $content.RuleName, $PSRuleReferenceUrl
+                } catch {
+                    Write-Warning ('Unable to build url for rule [{0}]' -f $content.RuleName)
+                    $resourceLink = $content.RuleName
+                }
+                $failContent += ('| {0} | `{1}` | {2} | ' -f $resourceLink, $content.TargetName, $content.Synopsis)
             }
-            $failContent += ('| {0} | `{1}` | {2} | ' -f $resourceLink, $content.TargetName, $content.Synopsis)
+            $failContent += [System.Collections.ArrayList]@(
+                '',
+                '</details>',
+                ''
+            )
+            # Append to output
+            Out-File -FilePath $outputFilePath -Append -NoClobber -InputObject $failContent
         }
-        $failContent += [System.Collections.ArrayList]@(
-            '',
-            '</details>',
-            ''
-        )
-        # Append to output
-        Out-File -FilePath $outputFilePath -Append -NoClobber -InputObject $failContent
-    }
 
-    if (($passedRules.Count -gt 0) -and -not $skipPassedRulesReport) {
-        # List of passed rules
-        $passContent = [System.Collections.ArrayList]@(
-            '',
-            '<details>',
-            '<summary>List of Passed Rules</summary>',
-            '',
-            '## Passed Rules',
-            '',
-            '| RuleName | TargetName |  Synopsis |',
-            '| :-- | :-- |  :-- |'
-        )
-        foreach ($content in $passedRules ) {
-            # Shorten the target name for deployment resoure type
-            if ($content.TargetType -eq 'Microsoft.Resources/deployments') {
-                $content.TargetName = $content.TargetName.replace('/home/runner/work/ResourceModules/ResourceModules/modules/', '')
+        if (($passedRules.Count -gt 0) -and -not $skipPassedRulesReport) {
+            # List of passed rules
+            $passContent = [System.Collections.ArrayList]@(
+                '',
+                '<details>',
+                '<summary>List of Passed Rules</summary>',
+                '',
+                '## Passed Rules',
+                '',
+                '| RuleName | TargetName |  Synopsis |',
+                '| :-- | :-- |  :-- |'
+            )
+            foreach ($content in $passedRules ) {
+                # Shorten the target name for deployment resoure type
+                if ($content.TargetType -eq 'Microsoft.Resources/deployments') {
+                    $content.TargetName = $content.TargetName.replace('/home/runner/work/ResourceModules/ResourceModules/modules/', '')
+                }
+
+                # Build hyperlinks to PSRule documentation for the rules
+                $TemplatesBaseUrl = 'https://azure.github.io/PSRule.Rules.Azure/en/rules'
+                try {
+                    $PSRuleReferenceUrl = '{0}/{1}' -f $TemplatesBaseUrl, $content.RuleName
+                    $null = Invoke-WebRequest -Uri $PSRuleReferenceUrl
+                    $resourceLink = '[{0}]({1})' -f $content.RuleName, $PSRuleReferenceUrl
+                } catch {
+                    Write-Warning 'Unable to build url for rule [{0}]' -f $content.RuleName
+                    $resourceLink = $content.RuleName
+                }
+                $passContent += ('| {0} | `{1}` | {2} |  ' -f $resourceLink, $content.TargetName, $content.Synopsis)
+
             }
-
-            # Build hyperlinks to PSRule documentation for the rules
-            $TemplatesBaseUrl = 'https://azure.github.io/PSRule.Rules.Azure/en/rules'
-            try {
-                $PSRuleReferenceUrl = '{0}/{1}' -f $TemplatesBaseUrl, $content.RuleName
-                $null = Invoke-WebRequest -Uri $PSRuleReferenceUrl
-                $resourceLink = '[{0}]({1})' -f $content.RuleName, $PSRuleReferenceUrl
-            } catch {
-                Write-Warning 'Unable to build url for rule [{0}]' -f $content.RuleName
-                $resourceLink = $content.RuleName
-            }
-            $passContent += ('| {0} | `{1}` | {2} |  ' -f $resourceLink, $content.TargetName, $content.Synopsis)
-
+            $passContent += [System.Collections.ArrayList]@(
+                '',
+                '</details>',
+                ''
+            )
+            # Append to output
+            Out-File -FilePath $outputFilePath -Append -NoClobber -InputObject $passContent
         }
-        $passContent += [System.Collections.ArrayList]@(
-            '',
-            '</details>',
-            ''
-        )
-        # Append to output
-        Out-File -FilePath $outputFilePath -Append -NoClobber -InputObject $passContent
     }
 }
