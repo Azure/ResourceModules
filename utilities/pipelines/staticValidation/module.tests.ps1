a0386e2fdd768d82876b0809652d21fb20a7f63a
AlexanderSehr <asehr@hotmail.de>
2023-05-31 21:18:36 +0200

    Added exclusions for tests

--- utilities/pipelines/staticValidation/module.tests.ps1
+++ utilities/pipelines/staticValidation/module.tests.ps1
@@ -826,10 +826,38 @@ Describe 'Test file tests' -Tag 'TestTemplate' {
             if (Test-Path $testFolderPath) {
                 $centralTestFilePath = Join-Path $testFolderPath 'main.test.bicep'
                 $testFilePaths = Get-ModuleTestFileList -ModulePath $moduleFolderPath | ForEach-Object { Join-Path $moduleFolderPath $_ }
+
+                # Collect scopes for special modules that deploy to a diverse set of scopes
+                $scopes = @()
+                foreach ($testFilePath in $testFilePaths) {
+                    $content = Get-Content -Path $testFilePath
+
+                    if (($content | Where-Object { $_ -like '*targetScope =*' }).Count -gt 0) {
+                        # Custom scope
+                        $scopeLine = $content | Where-Object { $_ -like '*targetScope =*' }
+                        if ($scopeLine -match "targetScope = '([a-zA-Z]+)'") {
+                            $scopes += @{
+                                path  = Split-Path (Split-Path $testFilePath -Parent) -Leaf
+                                scope = $Matches[1]
+                            }
+                        } else {
+                            throw "Unable to detect scope in file [$testFilePath]"
+                        }
+                    } else {
+                        # Default scope
+                        $scopes += @{
+                            path  = Split-Path (Split-Path $testFilePath -Parent) -Leaf
+                            scope = 'resourceGroup'
+                        }
+                    }
+                }
+
+                # Create test cases
                 foreach ($testFilePath in $testFilePaths) {
 
                     $deploymentTestFileTestCases += @{
                         testFilePath        = $testFilePath
+                        allTestScopes       = $scopes
                         testFileFolderName  = Split-Path (Split-Path $testFilePath -Parent) -Leaf
                         centralTestFilePath = $centralTestFilePath
                         moduleFolderName    = $moduleFolderPath.Replace('\', '/').Split('/modules/')[1]
@@ -851,10 +879,16 @@ Describe 'Test file tests' -Tag 'TestTemplate' {
 
             param(
                 [string] $testFileFolderName,
-                [string] $centralTestFilePath
+                [string] $centralTestFilePath,
+                [hashtable[]] $allTestScopes
             )
 
-            # TODO: Unless there all both scopes management group & subscription. Then use subscription. (Mgmt tests should be in status 'skipped')
+            $unsupportedScopeCombination = $allTestScopes.scope -contains 'subscription' -and $allTestScopes.scope -contains 'managementGroup'
+            $currentInManagementGroupScope = ($allTestScopes | Where-Object { $_.path -eq $testFileFolderName }).scope -eq 'managementGroup'
+            if ($unsupportedScopeCombination -and $currentInManagementGroupScope) {
+                Set-ItResult -Skipped -Because 'the module has tests in the management group as well as subscription scope. In these cases we only require the subscription scope in the [.test/main.test.bicep] file.'
+                return
+            }
 
             $centralTestFileContent = Get-Content $centralTestFilePath
             $centralTestFileTestCases = $centralTestFileContent | Where-Object {
