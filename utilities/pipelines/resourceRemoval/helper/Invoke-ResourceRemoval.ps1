54ec5a439a44b512de5b85c7258d64bd7409509c
AlexanderSehr <asehr@hotmail.de>
2023-08-11 08:41:07 +0200

    Updated preview removal flag

--- utilities/pipelines/resourceRemoval/helper/Invoke-ResourceRemoval.ps1
+++ utilities/pipelines/resourceRemoval/helper/Invoke-ResourceRemoval.ps1
@@ -154,7 +154,7 @@ function Invoke-ResourceRemoval {
             $subscriptionId = $resourceId.Split('/')[2]
             $resourceGroupName = $resourceId.Split('/')[4]
             $resourceName = Split-Path $resourceId -Leaf
-            $purgePath = 'subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.MachineLearningServices/workspaces/{2}?api-version=2023-04-01-preview&forceToPurge={3}' -f $subscriptionId, $resourceGroupName, $resourceName, $true
+            $purgePath = 'subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.MachineLearningServices/workspaces/{2}?api-version=2023-04-01&forceToPurge={3}' -f $subscriptionId, $resourceGroupName, $resourceName, $true
             $purgeRequestInputObject = @{
                 Method = 'DELETE'
                 Path   = $purgePath
