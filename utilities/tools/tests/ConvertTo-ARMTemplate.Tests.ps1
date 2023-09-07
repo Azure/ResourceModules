7dcaf6d3aa5e3fd105000b9848f83467045eef25
AlexanderSehr <asehr@hotmail.de>
2023-09-07 11:05:21 +0200

    Removed test for a parameter that isn't supported anymore

--- utilities/tools/tests/ConvertTo-ARMTemplate.Tests.ps1
+++ utilities/tools/tests/ConvertTo-ARMTemplate.Tests.ps1
@@ -87,59 +87,6 @@ Describe 'Test default behavior' -Tag 'Default' {
     }
 }
 
-Describe 'Test flag to including children' -Tag 'ConvertChildren' {
-
-    BeforeAll {
-        ConvertTo-ARMTemplate -RootPath $rootPath -ConvertChildren -Verbose -RunSynchronous
-    }
-
-    It 'All [<allBicepDeployFilesCount>] [main.bicep] files are converted to [main.json]' {
-        $deployJsonFilesCount = (Get-ChildItem -Recurse $modulesFolderPath | Where-Object { $_.FullName -match 'main.json' }).Count
-        $deployJsonFilesCount | Should -Be $allBicepDeployFilesCount
-    }
-
-    It 'All [<bicepTestFilesCount>] [main.test.bicep] files are converted to [main.test.json]' {
-        $deployJsonFilesCount = (Get-ChildItem -Recurse $modulesFolderPath | Where-Object { $_.FullName -match 'main.test.json' }).Count
-        $deployJsonFilesCount | Should -Be $bicepTestFilesCount
-    }
-
-    It 'All bicep files are removed' {
-        $actualBicepFilesCount = (Get-ChildItem -Recurse $modulesFolderPath | Where-Object { $_.FullName -match '.*.bicep' }).Count
-        $actualBicepFilesCount | Should -Be 0
-    }
-
-    It 'All json files have metadata removed' {
-        $releveantJSONFiles = (Get-ChildItem -Recurse $modulesFolderPath).FullName | Where-Object { $_ -match '.+(main.json|main.test.json)$' }
-
-        $metadataFound = $false
-
-        foreach ($deployJsonFile in $deployJsonFiles) {
-            $TemplateObject = Get-Content -Path $deployJsonFile -Raw | ConvertFrom-Json -AsHashtable
-
-            if ([bool]($TemplateObject.Keys -contains 'metadata')) {
-                $metadataFound = $true
-                break
-            }
-        }
-
-        $metadataFound | Should -Be $false
-    }
-
-    It 'All [<originalModuleWorkflowWithBicep>] GitHub workflow files are updated' {
-        $moduleWorkflowFilesUpdated = 0
-
-        foreach ($workFlowFile in $moduleWorkflowFiles) {
-            foreach ($line in (Get-Content -Path $workFlowFile.FullName)) {
-                if ($line -like '*templateFilePath:*.json*') {
-                    $moduleWorkflowFilesUpdated += 1
-                    break
-                }
-            }
-        }
-        $moduleWorkflowFilesUpdated | Should -Be $originalModuleWorkflowWithBicep
-    }
-}
-
 Describe 'Test flags that skip logic' -Tag 'Skip' {
 
     BeforeAll {
