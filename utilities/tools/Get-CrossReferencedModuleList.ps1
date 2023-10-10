2b928866abe9957053f570b9eabce5e675dd1d81
AlexanderSehr <asehr@hotmail.de>
2023-10-10 09:16:12 +0200

    Moved local-ref script back to pipes

--- utilities/tools/Get-CrossReferencedModuleList.ps1
+++ utilities/tools/Get-CrossReferencedModuleList.ps1
@@ -65,7 +65,7 @@ function Get-ReferenceObject {
         [string] $ModuleTemplateFilePath
     )
 
-    . (Join-Path $PSScriptRoot 'helper' 'Get-LocallyReferencedFileList.ps1')
+    . (Join-Path (Get-Item $PSScriptRoot).Parent 'pipelines' 'sharedScripts' 'Get-LocallyReferencedFileList.ps1')
     $involvedFilePaths = Get-LocallyReferencedFileList -FilePath $ModuleTemplateFilePath
 
     $resultSet = @{
