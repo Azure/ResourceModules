5dea5c6ba735cc7699f839dcea7e4b8346609469
AlexanderSehr <asehr@hotmail.de>
2023-10-15 12:35:54 +0200

    Made metadata removal ready for UDT

--- utilities/pipelines/staticValidation/helper/helper.psm1
+++ utilities/pipelines/staticValidation/helper/helper.psm1
@@ -155,9 +155,22 @@ function Remove-JSONMetadata {
         [hashtable] $TemplateObject
     )
     $TemplateObject.Remove('metadata')
-    for ($index = 0; $index -lt $TemplateObject.resources.Count; $index++) {
-        if ($TemplateObject.resources[$index].type -eq 'Microsoft.Resources/deployments') {
-            $TemplateObject.resources[$index] = Remove-JSONMetadata -TemplateObject $TemplateObject.resources[$index].properties.template
+
+    # Differantiate case: With user defined types (resources property is hashtable) vs without user defined types (resources property is array)
+    if ($TemplateObject.resources.GetType().BaseType.Name -eq 'Hashtable') {
+        # Case: Hashtable
+        $resourceIdentifiers = $TemplateObject.resources.Keys
+        for ($index = 0; $index -lt $resourceIdentifiers.Count; $index++) {
+            if ($TemplateObject.resources[$resourceIdentifiers[$index]].type -eq 'Microsoft.Resources/deployments') {
+                $TemplateObject.resources[$resourceIdentifiers[$index]] = Remove-JSONMetadata -TemplateObject $TemplateObject.resources[$resourceIdentifiers[$index]].properties.template
+            }
+        }
+    } else {
+        # Case: Array
+        for ($index = 0; $index -lt $TemplateObject.resources.Count; $index++) {
+            if ($TemplateObject.resources[$index].type -eq 'Microsoft.Resources/deployments') {
+                $TemplateObject.resources[$index] = Remove-JSONMetadata -TemplateObject $TemplateObject.resources[$index].properties.template
+            }
         }
     }
 
