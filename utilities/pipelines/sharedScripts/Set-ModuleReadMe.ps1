4ed2233cd83a495101e49c6ee6d5ce8b4d8c7dfe
AlexanderSehr <asehr@hotmail.de>
2023-11-20 09:17:05 +0100

    Performance Update 1

--- utilities/pipelines/sharedScripts/Set-ModuleReadMe.ps1
+++ utilities/pipelines/sharedScripts/Set-ModuleReadMe.ps1
@@ -1181,6 +1181,17 @@ function Set-UsageExamplesSection {
     ############################
     ##   Process test files   ##
     ############################
+
+    # Prepare data (using thread-safe multithreading) to consume later
+    $buildTestFileMap = [System.Collections.Concurrent.ConcurrentDictionary[string, object]]::new()
+    $testFilePaths | ForEach-Object -Parallel {
+        $folderName = Split-Path (Split-Path -Path $_) -Leaf
+        $buildTemplate = bicep build $_ --stdout | ConvertFrom-Json -AsHashtable
+
+        $dict = $using:buildTestFileMap
+        $null = $dict.TryAdd($folderName, $buildTemplate)
+    }
+
     $pathIndex = 1
     $usageExampleSectionHeaders = @()
     $testFilesContent = @()
@@ -1188,7 +1199,8 @@ function Set-UsageExamplesSection {
 
         # Read content
         $rawContentArray = Get-Content -Path $testFilePath
-        $compiledTestFileContent = bicep build $testFilePath --stdout | ConvertFrom-Json -AsHashtable
+        $folderName = Split-Path (Split-Path -Path $testFilePath) -Leaf
+        $compiledTestFileContent = $buildTestFileMap[$folderName]
         $rawContent = Get-Content -Path $testFilePath -Encoding 'utf8' | Out-String
 
         # Format example header
