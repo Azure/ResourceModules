10f3d181800ead7494cb0abaad4a451f644e82bd
AlexanderSehr <asehr@hotmail.de>
2024-01-02 00:40:05 +0100

    Update to latest

--- utilities/pipelines/resourceRemoval/Initialize-DeploymentRemoval.ps1
+++ utilities/pipelines/resourceRemoval/Initialize-DeploymentRemoval.ps1
@@ -78,7 +78,8 @@ function Initialize-DeploymentRemoval {
             'Microsoft.MachineLearningServices/workspaces',
             'Microsoft.Resources/resourceGroups',
             'Microsoft.Compute/virtualMachines',
-            'Microsoft.VirtualMachineImages/imageTemplates'
+            'Microsoft.VirtualMachineImages/imageTemplates',
+            'Microsoft.ManagedIdentity/userAssignedIdentities'
         )
 
         Write-Verbose ('Handling resource removal with deployment names [{0}]' -f ($deploymentNames -join ', ')) -Verbose
